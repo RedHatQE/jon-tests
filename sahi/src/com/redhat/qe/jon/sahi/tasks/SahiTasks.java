1484f6e273e1f1a5a6c81310a4f1a36359ae91ad
David Gao <dgao@redhat.com>
2011-05-31 17:24:19 -0400

    initial revision of Sahi testing using the java framework

---
+++ sahi/src/com/redhat/qe/jon/sahi/tasks/SahiTasks.java
@@ -0,0 +1,9 @@
+package com.redhat.qe.jon.sahi.tasks;
+
+import com.redhat.qe.auto.sahi.ExtendedSahi;
+
+public class SahiTasks extends ExtendedSahi {
+	public SahiTasks(String browserPath, String browserName, String browserOpt, String sahiBaseDir, String sahiUserdataDir) {
+		super(browserPath, browserName, browserOpt, sahiBaseDir, sahiUserdataDir);
+	}
+}
---
+++ sahi/src/com/redhat/qe/jon/sahi/tests/InventoryTest.java
@@ -0,0 +1,13 @@
+package com.redhat.qe.jon.sahi.tests;
+
+import com.redhat.qe.jon.sahi.base.SahiTestScript;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
+
+public class InventoryTest extends SahiTestScript{
+	@Test (groups="inventoryTest")
+	public void createCompatibleGroups() {
+		// yada yada yada
+	}
+}
