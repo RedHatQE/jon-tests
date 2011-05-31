ce653f901bc3bf095ed802008ebd798fc8b0370a
David Gao <dgao@redhat.com>
2011-05-31 17:44:23 -0400

    added simple create/delete inventory tests

--- sahi/src/com/redhat/qe/jon/sahi/tasks/SahiTasks.java
+++ sahi/src/com/redhat/qe/jon/sahi/tasks/SahiTasks.java
@@ -6,4 +6,23 @@ public class SahiTasks extends ExtendedSahi {
 	public SahiTasks(String browserPath, String browserName, String browserOpt, String sahiBaseDir, String sahiUserdataDir) {
 		super(browserPath, browserName, browserOpt, sahiBaseDir, sahiUserdataDir);
 	}
+
+	// ***************************************************************************
+	// Inventory
+	// ***************************************************************************
+	public void createGroup(String groupName, String groupDesc) {
+		this.link("Inventory").click();
+		this.cell("All Groups").click();
+		this.cell("New").click();
+		this.textbox("name").setValue(groupName);
+		this.textarea("description").setValue(groupDesc);
+		this.cell("Next").click();
+		this.cell("Finish").click();
+	}
+
+	public void deleteGroup(String groupName) {
+		this.div(groupName).click();
+		this.cell("Delete").click();
+		this.cell("Yes").click();
+	}
 }
--- sahi/src/com/redhat/qe/jon/sahi/tests/InventoryTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/InventoryTest.java
@@ -5,9 +5,30 @@ import com.redhat.qe.jon.sahi.base.SahiTestScript;
 import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 
+import com.redhat.qe.auto.testng.TestNGUtils;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 public class InventoryTest extends SahiTestScript{
-	@Test (groups="inventoryTest")
-	public void createCompatibleGroups() {
-		// yada yada yada
+	@Test (groups="inventoryTest", dataProvider="groupData")
+	public void createGroups(String groupName, String groupDesc) {
+		sahiTasks.createGroup(groupName, groupDesc);
+	}
+
+	@Test (groups="inventoryTest", dataProvider="groupData", dependsOnMethods={"createGroups"})
+	public void deleteGroups(String groupName, String groupDesc) {
+		sahiTasks.deleteGroup(groupName);
+	}
+
+	@DataProvider(name="groupData")
+	public Object[][] groupData() {
+		return TestNGUtils.convertListOfListsTo2dArray(getGroupData());
+	}
+	public List<List<Object>> getGroupData() {
+		ArrayList<List<Object>> data = new ArrayList<List<Object>>();
+		data.add(Arrays.asList(new Object[]{"hello_auto_world", "this is a test description"}));
+		return data;
 	}
 }
