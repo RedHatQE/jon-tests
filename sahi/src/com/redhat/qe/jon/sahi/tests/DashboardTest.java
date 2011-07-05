e399f8853af824306cf0d8e8db7ea8a6c865cfe0
Michael Foley <mfoley@redhat.com>
2011-07-05 13:14:15 -0400

    add new test

---
+++ sahi/src/com/redhat/qe/jon/sahi/tests/DashboardTest.java
@@ -0,0 +1,30 @@
+package com.redhat.qe.jon.sahi.tests;
+
+import org.testng.annotations.Test;
+
+import com.redhat.qe.jon.sahi.base.SahiTestScript;
+
+public class DashboardTest  extends SahiTestScript  {
+	
+	
+	@Test (groups="DashboardTest")
+	public void viewDashboard() {
+		//todo
+             		
+	}
+	
+	@Test (groups="DashboardTest")
+	public void editDashboard() {
+		//todo
+             		
+	}
+	
+	@Test (groups="DashboardTest")
+	public void newDashboard() {
+		//todo
+             		
+	}
+	
+	
+
+}
