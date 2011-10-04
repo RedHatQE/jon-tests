40c20cea784ffcc2c315d152417d338cf170fef0
Jan Martiska <jmartisk@redhat.com>
2011-10-04 14:29:17 +0200

    MonitoringTest for EAP plugin - domain

--- sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/domain/MonitoringTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/domain/MonitoringTest.java
@@ -1,4 +1,4 @@
-package com.redhat.qe.jon.sahi.tests.plugins.eap6.standalone;
+package com.redhat.qe.jon.sahi.tests.plugins.eap6.domain;
 
 import com.redhat.qe.jon.sahi.tests.plugins.eap6.AS7PluginSahiTasks;
 import com.redhat.qe.jon.sahi.tests.plugins.eap6.AS7PluginSahiTasks.Navigate;
@@ -16,12 +16,12 @@ public class MonitoringTest extends AS7PluginSahiTestScript {
     @BeforeClass(groups="monitoringTest")
     protected void setupAS7Plugin() {        
         as7SahiTasks = new AS7PluginSahiTasks(sahiTasks);
-        as7SahiTasks.inventorizeResourceByName(System.getProperty("agent.name"), System.getProperty("as7.standalone.name"));        
+        as7SahiTasks.inventorizeResourceByName(System.getProperty("agent.name"), System.getProperty("as7.domain.controller.name"));        
     }
     
     @Test(groups={"monitoringTest"})
-    public void disableMetricsTest()  {        
-        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.standalone.name"));               
+    public void disableMetricsTest_DomainController() {        
+        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.domain.controller.name"));               
         sahiTasks.xy(sahiTasks.cell("Schedules"), 3, 3).click();        
         // step1: disable one of the metrics 
         sahiTasks.xy(sahiTasks.cell("Maximum request time"), 3, 3).click();
@@ -50,8 +50,8 @@ public class MonitoringTest extends AS7PluginSahiTestScript {
     }
     
     @Test(groups={"monitoringTest"}, dependsOnMethods={"disableMetricsTest"})
-    public void enableMetricsTest()  {        
-        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.standalone.name"));                       
+    public void enableMetricsTest_DomainController()  {        
+        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.domain.controller.name"));                       
         // check that the metric is present in "Tables" tab                
         sahiTasks.xy(sahiTasks.cell("Tables"), 3, 3).click(); 
         try {
@@ -61,8 +61,8 @@ public class MonitoringTest extends AS7PluginSahiTestScript {
     }
     
     @Test(groups={"monitoringTest"})
-    public void specifyMonitoringIntervalTest() {        
-        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.standalone.name"));               
+    public void specifyMonitoringIntervalTest_DomainController() {        
+        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.domain.controller.name"));               
         sahiTasks.xy(sahiTasks.cell("Schedules"), 3, 3).click();                
         sahiTasks.xy(sahiTasks.cell("Maximum request time"), 3, 3).click();  
         
@@ -70,11 +70,6 @@ public class MonitoringTest extends AS7PluginSahiTestScript {
         textbox.setValue("4");        
         sahiTasks.cell("Set").click();
 
-        log.fine(sahiTasks.cell(3).in(sahiTasks.cell("Maximum request time").parentNode("TR")).fetch("innerHTML"));
-        log.fine(sahiTasks.cell(4).in(sahiTasks.cell("Maximum request time").parentNode("TR")).fetch("innerHTML"));        
         org.testng.Assert.assertTrue(sahiTasks.cell(4).in(sahiTasks.cell("Maximum request time").parentNode("TR")).getText().indexOf("4") != -1);        
-        
     }
-    
-    
 }
