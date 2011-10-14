ccd48bf13f46cea4e7ad61024b8e9559c1eaee22
Jeeva Kandasamy <jkandasa@redhat.com>
2011-10-14 09:57:36 +0530

    Removing driftTest class, it's duplicate

--- sahi/src/com/redhat/qe/jon/sahi/tests/DriftManagementTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/DriftManagementTest.java
@@ -87,7 +87,7 @@ public class DriftManagementTest extends SahiTestScript {
 		map.put(DRIFT_NAME, "File SystemDrift - monitor changes in file");
 		map.put(DRIFT_BASE_DIR, "/home/hudson/automationDriftManagementDir/");
 		map.put(DRIFT_TEXT_BOXES, "interval=60,valueName="+map.get(DRIFT_BASE_DIR));
-		map.put(DRIFT_RADIO_BUTTONS, "true,normal,fileSystem");
+		map.put(DRIFT_RADIO_BUTTONS, "enabled,normal,fileSystem");
 		//map.put(DRIFT_DIR_INCLUDES, "includeDir");
 		//map.put(DRIFT_DIR_EXCLUDES, "excludeDir");
 		map.put(DRIFT_FILE_INCLUDES, "includeDir");
