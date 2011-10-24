93260364d1f0b54de08156b03d4528e2be962a2f
Jeeva Kandasamy <jkandasa@redhat.com>
2011-10-24 17:00:18 +0530

    Removing unused code on agent/server plugins validation

--- sahi/src/com/redhat/qe/jon/sahi/tests/PluginsTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/PluginsTest.java
@@ -19,196 +19,22 @@ import org.testng.annotations.Test;
 		
 		//exact list of req'd plugins being provided by development 
 		
-		//////////////////////////////
-		// Agent Plugins
-		/////////////////////////////
-		
+		//*************************************
+		//* Agent Plugins validation
+		//*************************************
 		@Test (groups="PluginsTest", dataProvider="agentPluginsData")
 		public void validateAgentPlugins(String agentPluginsName, boolean pageRedirection){
 			Assert.assertTrue(sahiTasks.getAgentServerPluginsStaus(agentPluginsName, pageRedirection, true), "Agent Plugins '"+agentPluginsName+"' status");
 		}
 		
+		//*************************************
+		//* Server Plugins validation
+		//*************************************
 		@Test (groups="PluginsTest", dataProvider="serverPluginsData")
 		public void validateServerPlugins(String serverPluginsName, boolean pageRedirection){
 			Assert.assertTrue(sahiTasks.getAgentServerPluginsStaus(serverPluginsName, pageRedirection, false), "Server Plugins '"+serverPluginsName+"' status");
 		}
 		
-		@Test (groups="PluginsTest")
-		public void AntBundle() {
-			
-			sahiTasks.AntBundlePlugin();	
-		}
-		
-		
-		
-		@Test (groups="PluginsTest")
-		public void Augeas() {
-			
-			sahiTasks.AbstractAugeasPlugin();       		
-		}
-		
-		@Test (groups="PluginsTest")
-		public void ApacheHTTP() {
-			
-			sahiTasks.ApacheHTTPPlugin();   		
-		}
-		
-		@Test (groups="PluginsTest")
-		public void Database() {
-			
-			sahiTasks.AbstractDatabasePlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void IIS() {
-			
-			sahiTasks.IISPlugin();  		
-		}
-		
-		@Test (groups="PluginsTest")
-		public void JMX() {
-			
-			sahiTasks.GenericJMXPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void OS() {
-			
-			sahiTasks.OperatingSystemPlugin();   		
-		}
-		
-		
-		@Test (groups="PluginsTest")
-		public void PostgreSQL() {
-			
-			sahiTasks.PostGreSQLPlugin();  
-		}
-		
-		@Test (groups="PluginsTest")
-		public void RHQAgent() {
-			
-			sahiTasks.RHQAgentPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void Script() {
-			
-			sahiTasks.ScriptPlugin();
-		}
-		
-		/////////////////////////
-		// Server Plugins 
-		/////////////////////////
-		
-		@Test (groups="PluginsTest")
-		public void AntBundleProcessor() {
-			
-			sahiTasks.AntBundleProcessorPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void PerspectiveCore() {
-			
-			sahiTasks.PerspectiveCorePlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void DiskContent() {
-			
-			 sahiTasks.DiskContentPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void FileTemplateBundleProcessor() {
-			
-			 sahiTasks.FileTemplateBundleProcessorPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void JBossCSPContent() {
-			
-			 sahiTasks.JBossCSPContentPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void URLContent() {
-			
-			  sahiTasks.URLContentPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void YumContent() {
-			
-			 sahiTasks.YumContentPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void AlertCLI() {
-			
-			 sahiTasks.AlertCLIPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void AlertEmail() {
-			
-			 sahiTasks.AlertEmailPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void AlertIRC() {
-			
-			  sahiTasks.AlertIRCPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void AlertMicroblog() {
-			
-			 sahiTasks.AlertMicroBlogPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void Mobicents() {
-			
-			 sahiTasks.AlertMobicentsPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void AlertOperations() {
-			
-			sahiTasks.AlertOperationsPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void AlertRoles() {
-			
-			sahiTasks.AlertRolesPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void AlertSNMP() {
-			
-			 sahiTasks.AlertSNMPPlugin();
-		}
-		
-		
-		
-		@Test (groups="PluginsTest")
-		public void AlertSubject() {
-			
-			 sahiTasks.AlertSubjectPlugin();
-		}
-		
-		@Test (groups="PluginsTest")
-		public void DriftJPA() {
-			
-			 sahiTasks.DriftJPAPlugin();
-		}
-	    
-		@Test (groups="PluginsTest")
-		public void PackageTypeCLI() {
-			
-			 sahiTasks.PackageTypeCLIPlugin();
-		}
 		
 		
 		@DataProvider(name="agentPluginsData")
