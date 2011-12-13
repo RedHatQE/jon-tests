0649b867cd51a2937123a18b61a7ff8e8d23ff02
Libor Zoubek <lzoubek@redhat.com>
2011-12-13 17:04:48 +0100

    jon: added REST API test (groovy): visitAllResources

--- rest/testCases.groovy
+++ rest/testCases.groovy
@@ -1,5 +1,6 @@
 import org.testng.annotations.*
 import org.testng.TestNG
+import com.redhat.qe.auto.testng.Assert
 import org.testng.TestListenerAdapter
 // use cool mechanism to get http-builder dependency
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.5.1' )
@@ -7,17 +8,18 @@ import groovyx.net.http.RESTClient
 import groovy.util.slurpersupport.GPathResult
 import static groovyx.net.http.ContentType.URLENC
 
+
 /**
  * @author Libor Zoubek (lzoubek@redhat.com)
- * @since 12.12.2011
+ * @since 13.12.2011
  */
-class LoginTest {
+class RestClientTest {
 
-    private client
+    protected client
 
     @BeforeClass
-    def setUp() {
-	print '[jon.server.url] = '+System.properties['jon.server.url']
+    def baseSetUp() {
+	// print '[jon.server.url] = '+System.properties['jon.server.url']
 	client = new RESTClient( System.properties['jon.server.url'] + '/rest/1/' )
 	// check wheter JON is UP
 	try {
@@ -31,20 +33,97 @@ class LoginTest {
 	}
     }
 
+    def login() {
+    	client.auth.basic System.properties['rest.username'], System.properties['rest.password']
+    }
+
+    def acceptHeaderTest(resource) {
+    	client.get(path : resource, headers : [ Accept : 'application/xml' ]) { resp,xml ->	
+		Assert.assertTrue resp.status == 200, 'Response status is 200'
+		Assert.assertTrue resp.contentType == 'application/xml', 'reponse contentType is XML'
+		print xml.class
+	}
+    	client.get(path : resource, headers : [ Accept : 'application/json' ]) { resp,json ->	
+		Assert.assertTrue resp.status == 200, 'Response status is 200'
+		Assert.assertTrue resp.contentType == 'application/json', 'reponse contentType is JSON'
+		Assert.assertTrue json instanceof net.sf.json.JSON, 'response parsed to JSON object'
+	}
+    }
+}
+
+/**
+ * @author Libor Zoubek (lzoubek@redhat.com)
+ * @since 12.12.2011
+ */
+class LoginTest extends RestClientTest {
+
     @Test(expectedExceptions=groovyx.net.http.HttpResponseException.class)
     void loginUnauthorized() {
-    	client.get( path : 'status.json')
+    	client.get( path : 'status')
     }
 
     @Test(dependsOnMethods='loginUnauthorized',expectedExceptions=groovyx.net.http.HttpResponseException.class)
     void loginInvalidCredentials() {
     	client.auth.basic 'myUserName', 'myPassword'
-    	client.get( path : 'status.json')
+    	client.get( path : 'status')
     }
 
     @Test(dependsOnMethods='loginInvalidCredentials')
-    void login() {
-    	client.auth.basic System.properties['rest.username'], System.properties['rest.password']
-    	client.get( path : 'status.json')
+    void loginSuccessfull() {
+    	login()
+	client.get( path : 'status')
     }
 }
+
+/**
+ * @author Libor Zoubek (lzoubek@redhat.com)
+ * @since 13.12.2011
+ */
+class ResourceTest extends RestClientTest {
+
+	@BeforeClass
+	def setUp() {
+		login()
+	}
+	@Test
+	void platforms() {
+		acceptHeaderTest('resource/platforms')
+	}
+
+	void visitResource(href) {
+		println "VisitResource href=${href}"
+		client.get( path : href ) { resp, json -> 
+			println "Visited ${json.resourceId} : ${json.resourceName}"
+			json.links.each { link ->
+				// find link to resource's children
+				if (link.get('rel') == 'children') {
+					// list children
+					client.get( path : link.get('href')) { resp2, json2 ->
+						for (child in json2) {
+							println "Found child ${child.resourceId}"
+							// find each child link & recurse
+							child.links.each { l -> 
+								if (l.get('rel') == 'self') {
+									visitResource(l.get('href'))
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	@Test
+	void visitAllResources() {
+		client.get( path : 'resource/platforms.json' ) { resp, json ->
+			json.each {
+				it.links.each { link ->
+					if (link.get('rel') == 'self') {
+						visitResource(link.get('href'))
+					}
+				}
+			}
+		}
+	} 
+ }
