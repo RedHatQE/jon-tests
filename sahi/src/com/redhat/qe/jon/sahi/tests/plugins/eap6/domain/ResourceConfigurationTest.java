4c49be4b9d675d349190e6b4ee2ad194c7c1e0f2
Libor Zoubek <lzoubek@redhat.com>
2012-01-25 15:47:08 +0100

    rhq-eap: fix domain tests to pass on latest EAP6 plugin - managed servers are now child resources of DomainController, also move to new Navigation API

--- sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/domain/ResourceConfigurationTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/domain/ResourceConfigurationTest.java
@@ -1,5 +1,6 @@
 package com.redhat.qe.jon.sahi.tests.plugins.eap6.domain;
 
+import com.redhat.qe.jon.sahi.tasks.Navigator.InventoryNavigation;
 import com.redhat.qe.jon.sahi.tests.plugins.eap6.AS7PluginSahiTasks;
 import com.redhat.qe.jon.sahi.tests.plugins.eap6.AS7PluginSahiTasks.Navigate;
 import com.redhat.qe.jon.sahi.tests.plugins.eap6.AS7PluginSahiTestScript;
@@ -9,27 +10,29 @@ import org.testng.annotations.Test;
 
 /**
  *
- * @author jmartisk
+ * @author jmartisk & lzoubek
  */
 public class ResourceConfigurationTest extends AS7PluginSahiTestScript {
     
-    private boolean STATE_isDomainControllerInventorized = false;         
+    private InventoryNavigation navController;       
     
     @BeforeClass(groups="resourceConfiguration")
     protected void setupEapPlugin() {        
         as7SahiTasks = new AS7PluginSahiTasks(sahiTasks);
+        navController = new InventoryNavigation(System.getProperty("agent.name"),"Inventory",System.getProperty("as7.domain.controller.name"));
+        
         as7SahiTasks.inventorizeResourceByName(System.getProperty("agent.name"), System.getProperty("as7.domain.controller.name")); 
         as7SahiTasks.inventorizeResourceByName(System.getProperty("agent.name"), System.getProperty("as7.domain.host.server-one.name"));      
     }
      
     @Test(groups="resourceConfiguration")
     public void inventoryTest() {        
-        as7SahiTasks.assertResourceExistsInInventory(System.getProperty("agent.name"), System.getProperty("as7.domain.controller.name")); 
+        sahiTasks.assertResourceExists(true, navController);
     }
     
     @Test(groups={"resourceConfiguration"})
     public void predefinedMetricsOfHostControllerTest() {
-        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.domain.controller.name"));        
+        sahiTasks.getNavigator().inventoryGoToResource(navController);
         sahiTasks.xy(sahiTasks.cell("Schedules"), 3, 3).click();
         String[] predefinedMetrics = {
             "Maximum request time",
@@ -45,8 +48,8 @@ public class ResourceConfigurationTest extends AS7PluginSahiTestScript {
     }
     
     @Test(groups={"resourceConfiguration"})
-    public void predefinedMetricsOfManagedInstancesTest() {          
-        as7SahiTasks.navigate(Navigate.RESOURCE_MONITORING, System.getProperty("agent.name"), System.getProperty("as7.domain.host.server-one.name"));               
+    public void predefinedMetricsOfManagedInstancesTest() {                         
+        sahiTasks.getNavigator().inventoryGoToResource(navController.pathPush("as7.domain.host.server-one.name"));
         sahiTasks.xy(sahiTasks.cell("Schedules"), 3, 3).click();
         String[] predefinedMetrics = {
             "Server state"
