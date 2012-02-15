269391803f01459b4b63923c8c275a8b667d5f96
Jeeva Kandasamy <jkandasa@redhat.com>
2012-02-15 17:46:54 +0530

    adding base class

---
+++ clitest/src/com/redhat/qe/jon/clitest/base/CliTestScript.java
@@ -0,0 +1,47 @@
+package com.redhat.qe.jon.clitest.base;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import com.redhat.qe.auto.testng.TestScript;
+import com.redhat.qe.jon.clitest.tasks.CliTasks;
+import com.redhat.qe.jon.clitest.tests.CliTest;
+
+
+/**
+ * @author jkandasa@redhat.com (Jeeva Kandasamy)
+ * Feb 15, 2012
+ */
+public class CliTestScript extends TestScript{
+	private static Logger _logger = Logger.getLogger(CliTestScript .class.getName());
+	public CliTestScript(){
+		super();
+	}
+
+	@BeforeSuite
+	public void loadBeforeSuite() throws IOException{
+		_logger.log(Level.INFO, "Loading before Suite");
+		CliTasks.getCliTasks().getConnection(System.getenv().get("HOST_NAME"), System.getenv().get("HOST_USER"), System.getenv().get("HOST_PASSWORD"));
+		CliTest.cliShLocation = System.getenv().get("CLI_AGENT_BIN_SH");
+		CliTest.javaHome = System.getenv().get("JAVA_HOME").trim();
+		CliTest.rhqTarget = System.getenv().get("RHQ_TARGET").trim();
+		
+		CliTest.jsFileLocation = System.getProperty("user.dir")+"/"+System.getProperty("js.files.dir");
+		if(!CliTest.jsFileLocation.endsWith("/")){
+			CliTest.jsFileLocation += "/";
+		}
+		_logger.log(Level.INFO, "Loaded before Suite");
+	}
+
+	@AfterSuite
+	public void closeBrowser() {
+		_logger.log(Level.INFO, "Executing after Suite");
+		CliTasks.getCliTasks().closeConnection();
+		_logger.log(Level.INFO, "Completed after Suite");
+		
+	}
+}
--- clitest/src/com/redhat/qe/jon/clitest/tests/CliTest.java
+++ clitest/src/com/redhat/qe/jon/clitest/tests/CliTest.java
@@ -15,10 +15,10 @@ import com.redhat.qe.jon.clitest.tasks.CliTasksException;
 
 public class CliTest{
 	private static Logger _logger = Logger.getLogger(CliTest.class.getName());
-	private String cliShLocation;
-	private String jsFileLocation;
-	private String javaHome;
-	private String rhqTarget;
+	public static String cliShLocation;
+	public static String jsFileLocation;
+	public static String javaHome;
+	public static String rhqTarget;
 	private String cliUsername;
 	private String cliPassword;
 	private String makeFailure;
@@ -26,24 +26,12 @@ public class CliTest{
 		
 	
 	
-	@BeforeClass
-	public void loadBeforeClass() throws IOException{
-		cliTasks = CliTasks.getCliTasks();
-		cliTasks.getConnection(System.getenv().get("HOST_NAME"), System.getenv().get("HOST_USER"), System.getenv().get("HOST_PASSWORD"));
-		this.cliShLocation = System.getenv().get("CLI_AGENT_BIN_SH");
-		this.javaHome = System.getenv().get("JAVA_HOME").trim();
-		this.rhqTarget = System.getenv().get("RHQ_TARGET").trim();
-		
-		this.jsFileLocation = System.getProperty("user.dir")+"/"+System.getProperty("js.files.dir");
-		if(!jsFileLocation.endsWith("/")){
-			jsFileLocation += "/";
-		}
-	}
+	
 	
 	//@Parameters({"rhq.target","cli.username","cli.password","js.file","cli.args","expected.result","make.failure"})
 	public void loadSetup(@Optional String rhqTarget, @Optional String cliUsername, @Optional String cliPassword, @Optional String makeFailure) throws IOException{
 		if(rhqTarget != null)
-			this.rhqTarget = rhqTarget;
+			CliTest.rhqTarget = rhqTarget;
 		if(cliUsername != null)
 			this.cliUsername = cliUsername;
 		if(cliPassword != null)
@@ -56,7 +44,7 @@ public class CliTest{
 	@Test
 	public void runJSfile(@Optional String rhqTarget, @Optional String cliUsername, @Optional String cliPassword, String jsFile, @Optional String cliArgs, @Optional String expectedResult, @Optional String makeFilure) throws IOException, CliTasksException{
 		loadSetup(rhqTarget, cliUsername, cliPassword, makeFilure);
-		String output = cliTasks.runCommnad("export RHQ_CLI_JAVA_HOME="+javaHome+";"+this.cliShLocation+" -s "+this.rhqTarget+" -u "+this.cliUsername+" -p "+this.cliPassword+" -f "+jsFileLocation+jsFile);
+		String output = cliTasks.runCommnad("export RHQ_CLI_JAVA_HOME="+javaHome+";"+CliTest.cliShLocation+" -s "+CliTest.rhqTarget+" -u "+this.cliUsername+" -p "+this.cliPassword+" -f "+jsFileLocation+jsFile);
 		_logger.log(Level.INFO, output);
 	}	
 }
