31d89f50c1d513c7e229794ccf39022a2c8d3a9e
Libor Zoubek <lzoubek@redhat.com>
2012-03-16 17:34:44 +0100

    rhq-eap: forgotten testing code

--- sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/JMSDestinationsTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/JMSDestinationsTest.java
@@ -1,7 +1,5 @@
 package com.redhat.qe.jon.sahi.tests.plugins.eap6.standalone;
 
-import junit.framework.Assert;
-
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -21,10 +19,6 @@ public class JMSDestinationsTest extends AS7StandaloneTest {
         hornetq = server.child("messaging").child("default");
         queue = hornetq.child("test-queue");
         topic = hornetq.child("test-topic");
-        for (Resource child : hornetq.getChildrenTree()) {
-        	log.info(child.toString());
-        }
-        Assert.fail();
     }
 	@Test(groups="jms")	
 	public void addQueue() {
