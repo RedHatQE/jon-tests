825a9386f4c3ee5a716d0426ad310ac46476afb3
Libor Zoubek <lzoubek@redhat.com>
2012-04-24 22:56:56 +0200

    rhq-eap: pass valid suffix

--- sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/LoggingSubsystem.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/LoggingSubsystem.java
@@ -110,7 +110,7 @@ public class LoggingSubsystem extends AS7StandaloneTest {
 		op.getEditor().setText("resourceName", prfHandler.getName());
 		op.next();
 		op.getEditor().setText("path", "standalone/log/test.log");
-		op.getEditor().setText("suffix", "foo");
+		op.getEditor().setText("suffix", "HH");
 		op.finish();
 		inventory.childHistory().assertLastResourceChange(true);
 		mgmtClient.assertResourcePresence("/subsystem=logging", "periodic-rotating-file-handler", prfHandler.getName(),true);
