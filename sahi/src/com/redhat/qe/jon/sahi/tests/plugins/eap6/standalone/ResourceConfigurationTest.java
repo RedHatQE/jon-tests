fc939e630555b257a27f6371320a0868977ac511
Libor Zoubek <lzoubek@redhat.com>
2012-04-25 11:39:30 +0200

    rhq-eap: added deployment-scanner tests

--- sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/ResourceConfigurationTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/ResourceConfigurationTest.java
@@ -4,6 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.redhat.qe.jon.sahi.base.inventory.Configuration;
+import com.redhat.qe.jon.sahi.base.inventory.Configuration.ConfigEntry;
+import com.redhat.qe.jon.sahi.base.inventory.Configuration.CurrentConfig;
+import com.redhat.qe.jon.sahi.base.inventory.Resource;
 import com.redhat.qe.jon.sahi.tasks.Timing;
 
 /**
@@ -15,11 +19,15 @@ import com.redhat.qe.jon.sahi.tasks.Timing;
  */
 public class ResourceConfigurationTest extends AS7StandaloneTest {
     
-    @BeforeClass(groups={"configuration","inventoryTest"})
+	private Resource deploymentScanner;
+	private String deployDir="deployDir2";
+	
+    @BeforeClass()
     protected void setupAS7Plugin() {        
         as7SahiTasks.importResource(server);        
         log.finer("Waiting "+Timing.toString(Timing.TIME_30S)+" till server is properly inventorized");
         sahiTasks.waitFor(Timing.TIME_30S);
+        deploymentScanner = server.child("deployment-scanner");
         Assert.assertTrue(server.isAvailable(), 
                 "Resource " + server.toString() + " should be ONLINE, but I could not verify this!");
     }
@@ -28,15 +36,51 @@ public class ResourceConfigurationTest extends AS7StandaloneTest {
      * Tries to inventorize a standalone AS7 instance and then verifies that it appears in the inventory of the agent.
      * @see TCMS test case 96428
      */
-    @Test(groups={"inventoryTest"})
+    @Test()
     public void inventoryTest() {
         server.assertExists(true);
+    }
+    @Test()
+    public void addDeploymentScanner() {
+    	sshClient.run("mkdir -p "+sshClient.getAsHome()+"/standalone/"+deployDir);
+    	Configuration configuration = deploymentScanner.configuration();
+    	CurrentConfig config = configuration.current();
+    	ConfigEntry ce = config.newEntry(0);
+    	ce.getEditor().setText("name", deployDir);
+    	ce.getEditor().setText("path", deployDir);
+    	ce.getEditor().checkBox(0, false);
+    	ce.getEditor().setText("relative-to", "jboss.server.base.dir");
+    	ce.OK();
+    	config.save();
+    	configuration.history().failOnFailure();
+    	mgmtClient.assertResourcePresence("/subsystem=deployment-scanner", "scanner", deployDir, true);
+    	Assert.assertTrue(mgmtClient.readAttribute("/subsystem=deployment-scanner/scanner="+deployDir, "path").get("result").asString().equals(deployDir),"Deployment scanner has correctly set PATH");
+    }
+    @Test(dependsOnMethods="addDeploymentScanner")
+    public void configureDeploymentScanner() {
+    	Configuration configuration = deploymentScanner.configuration();
+    	CurrentConfig config = configuration.current();
+    	ConfigEntry ce = config.getEntry(deployDir);
+    	ce.getEditor().checkRadio("scan-enabled[1]");
+    	ce.OK();
+    	config.save();
+    	configuration.history().failOnFailure();
+    	Assert.assertFalse(mgmtClient.readAttribute("/subsystem=deployment-scanner/scanner="+deployDir, "scan-enabled").get("result").asBoolean(),"Deployment scanner configuratio updated - scanner is disabled");
+    }
+    @Test(dependsOnMethods="configureDeploymentScanner")
+    public void removeDeploymentScanner() {
+    	Configuration configuration = deploymentScanner.configuration();
+    	CurrentConfig config = configuration.current();
+    	config.removeEntry(deployDir);
+    	config.save();
+    	configuration.history().failOnFailure();
+    	mgmtClient.assertResourcePresence("/subsystem=deployment-scanner", "scanner", deployDir, false);
     }
     
     /**
      * @see TCMS testcase 96429
      */
-    @Test(groups={"inventoryTest"}, dependsOnMethods={"inventoryTest"})
+    @Test(dependsOnMethods={"inventoryTest"})
     public void predefinedMetricsTest() {               
         server.monitoring();
         sahiTasks.xy(sahiTasks.cell("Schedules"), 3, 3).click();
