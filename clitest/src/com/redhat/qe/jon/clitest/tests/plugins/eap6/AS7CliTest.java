390cfa1468b636e28402665ec9db03a08f9d1604
Libor Zoubek <lzoubek@redhat.com>
2012-05-28 17:02:53 +0200

    JON EAP6CLI: improved logging when server does not start

--- clitest/src/com/redhat/qe/jon/clitest/tests/plugins/eap6/AS7CliTest.java
+++ clitest/src/com/redhat/qe/jon/clitest/tests/plugins/eap6/AS7CliTest.java
@@ -3,13 +3,16 @@ package com.redhat.qe.jon.clitest.tests.plugins.eap6;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 
+import org.jboss.sasl.util.UsernamePasswordHashUtil;
 import org.testng.annotations.BeforeSuite;
 
 import com.redhat.qe.auto.testng.Assert;
 import com.redhat.qe.jon.clitest.tasks.CliTasksException;
 import com.redhat.qe.jon.clitest.tests.CliTest;
 import com.redhat.qe.jon.clitest.tests.plugins.eap6.ServerStartConfig.ConfigFile;
+import com.redhat.qe.jon.common.util.AS7DMRClient;
 import com.redhat.qe.jon.common.util.AS7SSHClient;
 
 public class AS7CliTest extends CliTest {
@@ -42,6 +45,50 @@ public class AS7CliTest extends CliTest {
 		domainHome = System.getProperty("as7.domain.home");
 		domainHostName = System.getProperty("as7.domain.hostname");
 	}
+	
+	/**
+	 * installs default RHQ user for given server
+	 * if 'server' param exists in RHQ UI, 10min waiting is started  
+	 * @param sshClient AS7SSHClient that can work with 'server' resource
+	 * @param mgmtClient Management client that can manage 'server' resource (can be null)
+	 * @param credFile relative path within {@link AS7SSHClient#getAsHome()} to save user name + hashed pass
+	 */
+	 public static void installRHQUser(AS7SSHClient sshClient, AS7DMRClient mgmtClient, String credFile) {	
+		String user = "rhqadmin";
+		String checkCmd = "grep '"+user+"' "+sshClient.getAsHome() + credFile;
+		if (sshClient.runAndWait(checkCmd).getStdout().contains(user)) {
+			log.info("rhqadmin already exists");
+			return;
+		}
+		String pass = "rhqadmin";
+		String hash = null;
+		// we also add default user admin:admin
+		String defaultUser = "admin";
+		String defaultHash = null;
+		// let's generate hash for pass and store it into mgmt-users.properties
+		try {
+			hash = new UsernamePasswordHashUtil().generateHashedHexURP(user,"ManagementRealm", pass.toCharArray());
+			defaultHash = new UsernamePasswordHashUtil().generateHashedHexURP(defaultUser,"ManagementRealm", "admin".toCharArray());
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(
+					"Unable to generate password hash to setup EAP", e);
+		}
+		
+		StringBuilder command = new StringBuilder("echo " + defaultUser + "=" + defaultHash + " > ");
+		command.append(sshClient.getAsHome() + credFile);
+		sshClient.runAndWait(command.toString());
+		log.info("Created default user:pass "+defaultUser+":admin");
+		
+		command = new StringBuilder("echo " + user + "=" + hash + " >> ");
+		command.append(sshClient.getAsHome() + credFile);
+		sshClient.runAndWait(command.toString());
+		log.info("Created testing user:pass "+user+":"+pass);	
+		if (mgmtClient!=null) {	
+			mgmtClient.setUsername("rhqadmin");
+			mgmtClient.setPassword("rhqadmin");
+		}
+	}	
+	
 	public void waitFor(int ms) {
 		waitFor(ms, "Waiting");
 	}
@@ -71,7 +118,14 @@ public class AS7CliTest extends CliTest {
 		}
 		sshClient.restart(params);
 		waitFor(30*1000,"Waiting until EAP starts up");
-		Assert.assertTrue(sshClient.isRunning(), "Server process is running");
+		boolean running = sshClient.isRunning();
+		if (!running) {
+			// EAP failed to start, lets print its logs
+			sshClient.runAndWait("cat "+sshClient.getAsHome()+"/standalone/log/boot.log");
+			sshClient.runAndWait("cat "+sshClient.getAsHome()+"/standalone/log/server.log");
+			sshClient.runAndWait("cat "+sshClient.getAsHome()+"/domain/log/host-controller.log");
+		}
+		Assert.assertTrue(running, "Server process is running");
 		sshClient.runAndWait("netstat -pltn | grep java");
 		runJSfile(null, "rhqadmin", "rhqadmin", jsFile, "--args-style=named agent="+agentName, start.getExpectedMessage(), null);
 	}
