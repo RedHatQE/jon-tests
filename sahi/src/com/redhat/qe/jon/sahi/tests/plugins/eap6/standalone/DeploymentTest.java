422488569ef023319d1de63beab3a22ec21529e5
Libor Zoubek <lzoubek@redhat.com>
2012-05-30 14:05:23 +0200

    JON EAP6UI: added deployment test containing MDB

--- sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/DeploymentTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/plugins/eap6/standalone/DeploymentTest.java
@@ -1,11 +1,19 @@
 package com.redhat.qe.jon.sahi.tests.plugins.eap6.standalone;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.redhat.qe.jon.sahi.base.inventory.Inventory;
 import com.redhat.qe.jon.sahi.base.inventory.Inventory.ChildResources;
 import com.redhat.qe.jon.sahi.base.inventory.Inventory.NewChildWizard;
+import com.redhat.qe.jon.sahi.base.inventory.Operations;
+import com.redhat.qe.jon.sahi.base.inventory.Operations.Operation;
 import com.redhat.qe.jon.sahi.base.inventory.Resource;
 import com.redhat.qe.jon.sahi.tasks.Timing;
 /**
@@ -21,51 +29,100 @@ public class DeploymentTest extends AS7StandaloneTest {
 	private static final String war = "hello.war";
 	private Resource wsResource;
 	private Resource warResource;
+	private Resource mdbResource;
 	@BeforeClass()
     protected void setupAS7Plugin() {
 		as7SahiTasks.importResource(server);
-		warResource = server.child(war);
+		warResource = server.child("hello.war");
 		wsResource = server.child("ws.war");
+		mdbResource = server.child("mdb.war");
     }
 
 	@Test()
-	public void deployWAR() {
-		if (mgmtClient.existsResource("", "deployment", war)) {
-			removeDeployment(war);
-			log.fine("Deployment removed using API, we have perform manual discovery for ds to disappear from RHQ UI");
-			log.info("manual discovery");
-			server.performManualAutodiscovery();
-		}
-		Inventory inventory = server.inventory();
-		ChildResources childResources = inventory.childResources();
-		NewChildWizard newChild = childResources.newChild("Deployment");
-		newChild.next();
-		newChild.upload("deploy/original/"+war);
-		//wait for upload to finish
-		sahiTasks.waitFor(2*waitTime);
-		newChild.next();
-		newChild.finish();
-		assertDeploymentExists(war);
-		warResource.assertExists(true);
+	public void deployWAR() {		
+		deployWARFile(warResource, "deploy/original/"+warResource.getName());
 		httpClient.assertDeploymentContent(war,"Original","Check whether original version of WAR has been deployed");
 	}
-	
-	@Test
-	public void deployWebService() {
+	private void deployWARFile(Resource war, String srcPath) {
 		Inventory inventory = server.inventory();
 		ChildResources childResources = inventory.childResources();
 		NewChildWizard newChild = childResources.newChild("Deployment");
 		newChild.next();
-		newChild.upload("deploy/"+wsResource.getName());
+		newChild.upload(srcPath);
 		//wait for upload to finish
 		sahiTasks.waitFor(2*waitTime);
 		newChild.next();
 		newChild.finish();
-		assertDeploymentExists(war);
-		wsResource.assertExists(true);
+		assertDeploymentExists(war.getName());
+		war.assertExists(true);
+	}
+	@Test
+	public void deployWebService() {
+		deployWARFile(wsResource, "deploy/"+wsResource.getName());
 		wsResource.child("webservices").assertExists(true);
 		wsResource.child("webservices").child("ws%3AHelloWorld").assertExists(true);
 	}
+	@Test
+	public void deployMessageDrivenBean() {
+		deployWARFile(wsResource, "deploy/"+mdbResource.getName());
+		mdbResource.child("messaging").assertExists(true);
+		mdbResource.child("messaging").child("default").assertExists(true);
+		mdbResource.child("messaging").child("default").child("HELLOWORLDMDBQueue").assertExists(true);
+	}
+	
+	@DataProvider
+	public Object[][] messagingSubsystemOperations() {
+		List<OpDef> defs = new ArrayList<OpDef>();		
+		defs.add(new OpDef("List Connection IDs"));
+		defs.add(new OpDef("List Producers Info as JSON"));
+		defs.add(new OpDef("List Connections as JSON"));
+		defs.add(new OpDef("List Heuristic Committed Transactions"));
+		defs.add(new OpDef("List Prepared Transaction JMS Details as JSON"));
+		Object[][] output = new Object[defs.size()][];
+		
+		for (int i=0;i<defs.size();i++) {
+			output[i] = new Object[] {defs.get(i)};
+		}		
+		return output;
+	}
+	
+	@Test(dependsOnMethods="deployMessageDrivenBean",dataProvider="messagingSubsystemOperations")
+	public void deployedMessagingSubsystemOperations(OpDef opDef) {
+		Resource hornetInstance = mdbResource.child("messaging").child("default");
+		Operations operations = hornetInstance.operations();
+		Operation op = operations.newOperation(opDef.getName());
+		op.schedule();
+		operations.assertOperationResult(op, true);
+	}
+	
+	@DataProvider
+	public Object[][] jmsQueueOperations() {
+		List<OpDef> defs = new ArrayList<OpDef>();		
+		defs.add(new OpDef("List Consumers as JSON"));
+		defs.add(new OpDef("List Message Counter History as HTML"));
+		defs.add(new OpDef("List Message Counter History as JSON"));		
+		defs.add(new OpDef("List Message Counter as HTML"));
+		defs.add(new OpDef("List Message Counter as JSON"));
+		defs.add(new OpDef("List Consumers as JSON"));
+		defs.add(new OpDef("List Messages"));
+		defs.add(new OpDef("List Messages as JSON"));
+		Object[][] output = new Object[defs.size()][];
+		
+		for (int i=0;i<defs.size();i++) {
+			output[i] = new Object[] {defs.get(i)};
+		}		
+		return output;
+	}
+	
+	@Test(dependsOnMethods="deployMessageDrivenBean",dataProvider="jmsQueueOperations")
+	public void deployedJMSQueueOperations(OpDef opDef) {
+		Resource jmsQueue = mdbResource.child("messaging").child("default").child("HELLOWORLDMDBQueue");
+		Operations operations = jmsQueue.operations();
+		Operation op = operations.newOperation(opDef.getName());
+		op.schedule();
+		operations.assertOperationResult(op, true);
+	}
+	
 	
 	// TODO re-deployment must be done using Content subsystem
 	//@Test(groups = {"deployment","blockedByBug-767974"}, dependsOnMethods="deployWAR")
@@ -99,14 +156,21 @@ public class DeploymentTest extends AS7StandaloneTest {
 		mgmtStandalone.assertResourcePresence("", "deployment", name, false);
 	}
 
-	/**
-	 * removes deployment
-	 * @param name
-	 */
-	private void removeDeployment(String name) {
-		log.info("remove datasource API");
-		if (mgmtStandalone.executeOperationVoid("/deployment="+name, "remove", new String[]{})) {
-			log.info("[mgmt API] Deployment was removed");
+	public static class OpDef {
+		private final String name;
+		private final Map<String,String> params = new LinkedHashMap<String, String>();
+		public OpDef(String name) {
+			this.name=name;
+		}
+		public Map<String, String> getParams() {
+			return params;
+		}
+		public String getName() {
+			return name;
+		}
+		@Override
+		public String toString() {
+			return "OperationName="+getName();
 		}
 	}
 }
