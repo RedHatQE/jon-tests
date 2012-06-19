beee3ed98c06f3871031e9abd1330dd11cec3818
Libor Zoubek <lzoubek@redhat.com>
2012-06-19 22:20:34 +0200

    JON CLI: commonmodule.js API update

--- clitest/resources/js-files/commonmodule.js
+++ clitest/resources/js-files/commonmodule.js
@@ -16,6 +16,11 @@ var _common = function() {
 	};
 	
 	return {
+		objToString : function(obj) {
+			var str=""; 
+			for (var k in obj) {if (obj.hasOwnProperty(k)) str=str.concat(k+"="+obj[k]+",");}
+			return str.substring(0,str.length-1);
+		},
 		pageListToArray : function(pageList) {
 			var resourcesArray = new Array();
 		    var i = 0;
@@ -23,7 +28,7 @@ var _common = function() {
 		    	resourcesArray[i] = pageList.get(i);
 		    }
 		    return resourcesArray;
-		},		
+		},
 		waitFor : function(conditionFunc) {
 			var time = 0;
 			var timeout = 3;
@@ -35,53 +40,78 @@ var _common = function() {
 		},
 		info : function(message) {
 			if (typeof verbose == "number" && verbose<=0) {
-				_println(message);
+				_println("[INFO] "+message);
 			}
 		},
 		debug : function(message) {
 			if (typeof verbose == "number" && verbose>=1) {
-				_println(message);
+				_println("[DEBUG] "+message);
 			}
 		},
 		trace : function(message) {
 			if (typeof verbose == "number" && verbose>=2) {
-				_println(message);
+				_println("[TRACE] "+message);
 			}
 		}
 	};
 };
 
+
 var Inventory = (function () {
 	var common = new _common();
 	return {
-		createCriteria : function(status,resName,parentName,resTypeName, resVersion, resCategory) {
-			common.trace("Inventory.createCriteria(status="+status+" resName="+resName+" parentName="+parentName+" resTypeName="+resTypeName+" resVersion="+resVersion+" resCategory="+resCategory+")");
-			var criteria = new ResourceCriteria();
-		    criteria.addFilterInventoryStatus(status);
-		    if (resName!=null)
-		    	criteria.addFilterName(resName);
-		    if(parentName!=null)
-		    	criteria.addFilterParentResourceName(parentName);
-		    if(resTypeName != null)
-		    	criteria.addFilterResourceTypeName(resTypeName);
-		    if(resVersion != null)
-		    	criteria.addFilterVersion(resVersion);
-		    if(resCategory != null)
-		    	criteria.addFilterResourceCategories(resCategory);
-		    return criteria;
+		createCriteria : function(params) {
+			common.trace("Inventory.createCriteria("+common.objToString(params) +")");
+			var criteria = new ResourceCriteria();	
+			for (var k in params) {
+			    // use hasOwnProperty to filter out keys from the Object.prototype
+			    if (params.hasOwnProperty(k)) {
+			    	if (k=="status") {
+			    		 eval("criteria.addFilterInventoryStatus(InventoryStatus."+params[k]+")");
+			    		 continue;
+			    	}
+			    	if (k=="category") {
+			    		eval("criteria.addFilterResourceCategories(ResourceCategory."+params[k]+")");
+			    		 continue;
+			    	}
+			    	if (k=="availability") {
+			    		eval("criteria.addFilterCurrentAvailability(AvailabilityType."+params[k]+")");
+			    		continue;
+			    	}
+			        var key = k[0].toUpperCase()+k.substring(1);        
+			        var func = eval("criteria.addFilter"+key);
+			        if (typeof func !== "undefined") {
+			        	func.call(criteria,params[k]);
+			        }
+			        else {
+			        	throw "Parameter ["+k+"] is not valid filter parameter";
+			        }
+			    }
+			}
+			// by default only 200 items are returned, this line discards it .. so we get unlimited list
+			criteria.clearPaging();
+			return criteria;
 		},
-		
-		find : function(resName,parentName,resTypeName, resVersion, resCategory) {    
-			common.trace("Inventory.find(resName="+resName+" parentName="+parentName+" resTypeName="+resTypeName+" resVersion="+resVersion+" resCategory="+resCategory+")");
-			var criteria = Inventory.createCriteria(InventoryStatus.COMMITTED,resName,parentName,resTypeName,resVersion,resCategory);
+		/**
+		 * finds resources in inventory
+		 * @param params
+		 * @returns array of resources
+		 */
+		find : function(params) {    
+			common.trace("Inventory.find("+common.objToString(params)+")");
+			params.status="COMMITTED";
+			var criteria = Inventory.createCriteria(params);
 			var resources = ResourceManager.findResourcesByCriteria(criteria);
-			common.debug("Found resources "+resources);
+			common.debug("Found "+resources.size()+" resources ");
 		    return common.pageListToArray(resources).map(function(x){return new Resource(x);});
 		},
-		
+		/**
+		 * 
+		 * @returns array of platforms in inventory
+		 */
 		platforms : function() {
 			common.trace("Inventory.platforms()");
-			return Inventory.find(null,null,null,null,ResourceCategory.PLATFORM);
+			return Inventory.find({category:"PLATFORM"});
 		}
 	};
 }) ();
@@ -102,54 +132,56 @@ Inventory.discoveryQueue = (function () {
 	    return resources;
 	};
 	
-	var _importResources = function (resName,parentName,resTypeName, resVersion, resCategory){
-		common.trace("discoveryQueue._importResources(resName="+resName+" parentName="+parentName+" resTypeName="+resTypeName+" resVersion="+resVersion+" resCategory="+resCategory+")");
-		var criteria = Inventory.createCriteria(InventoryStatus.NEW,resName,parentName,resTypeName,resVersion,resCategory);	    
+	var _importResources = function (json){
+		common.trace("discoveryQueue._importResources("+common.objToString(criteria)+")");
+		json.status="NEW";
+		var criteria = Inventory.createCriteria(json);	    
 	    common.info("Waiting until desired resources become NEW");
 	    var resources = _waitForResources(criteria);
-	    common.debug("Found NEW resources "+resources);	    
+	    common.debug("Found "+resources.size()+" NEW resources");	    
 	    var resourcesArray = common.pageListToArray(resources);
-	    common.debug("Resources about to Import: " + resourcesArray);
 	    assertTrue(resources.size()>0, "At least one resrouce was found");
 	    DiscoveryBoss.importResources(resourcesArray.map(function(x){return x.id;}));
-	    
-	    // we need to wait 'till resources are committed
-	    criteria = Inventory.createCriteria(InventoryStatus.COMMITTED,resName,parentName,resTypeName,resVersion,resCategory);	   
+	    json.status="COMMITTED";
+	    criteria = Inventory.createCriteria(json);	   
 	    common.info("Waiting until resources become COMMITTED");
-	    var committed = _waitForResources(criteria);
-	    
-	    assertTrue(committed.size() == resources.size(), "Size of COMMITED resources equals size of NEW before import");   
-	    return committed;
+	    var committed = _waitForResources(criteria);	    
+	    assertTrue(committed.size() > 0, "COMMITED resources size > 0");   
+	    return common.pageListToArray(committed).map(function(x){return new Resource(x);});
 	};
 	
 	return {
 		list : function () {
 			common.trace("discoveryQueue.list()");
-			var criteria = Inventory.createCriteria(InventoryStatus.NEW,null,null,null,null,null);
+			var criteria = Inventory.createCriteria({status:"NEW"});
 			var resources = ResourceManager.findResourcesByCriteria(criteria);
 			return common.pageListToArray(resources);
 		},
 		listPlatforms : function listPlatforms() {
 			common.trace("discoveryQueue.listPlatforms()");
-			var criteria = Inventory.createCriteria(InventoryStatus.NEW,null,null,null,null,ResourceCategory.PLATFORM);
+			var criteria = Inventory.createCriteria({status:"NEW",category:"PLATFORM"});
 			var resources = ResourceManager.findResourcesByCriteria(criteria);
 			return common.pageListToArray(resources);
 		},
-		importPlatform: function(name) {
-			common.trace("discoveryQueue.importPlatform(name="+name+")");
+		importPlatform: function(name,children) {
+			common.trace("discoveryQueue.importPlatform(name="+name+" children[default=true]="+children+")");
+			children = children || true;
 			// first lookup whether platform is already imported
-			var resources = Inventory.find(name,null,null,null,ResourceCategory.PLATFORM);
+			var resources = Inventory.find({name:name,category:"PLATFORM"});
 			if (resources.length == 1) {
 				return resources[0];
 			}
-			resources = _importResources(name,null,null,null,ResourceCategory.PLATFORM);
-			assertTrue(resources.size() == 1, "Plaform was not imported");
-			return new Resource(resources.get(0));
+			resources = _importResources({name:name,category:"PLATFORM"});
+			assertTrue(resources.length == 1, "Plaform was not imported");
+			if (children) {
+				_importResources({parentResourceId:resources[0].getId()});
+			}
+			return resources[0];
 		},
 		importResource : function(resource) {
 			if (!resource.exists()) {
 				DiscoveryBoss.importResources([resource.getId()]);
-				waitFor(resource.exists());
+				waitFor(resource.exists);
 			}
 		}
 	};
@@ -166,9 +198,7 @@ var Resource = function (param) {
 	var _res = param;
 	
 	var find = function() {
-		var criteria = new ResourceCriteria();
-		criteria.addFilterId(_id);
-		criteria.addFilterInventoryStatus(InventoryStatus.COMMITTED);
+		var criteria = Inventory.createCriteria({id:_id});
 		var resources = ResourceManager.findResourcesByCriteria(criteria);
 		common.info("Resource.find: "+resources);
 		return resources;
@@ -181,6 +211,9 @@ var Resource = function (param) {
 		}
 		return found.get(0).getCurrentAvailability().getAvailabilityType() == AvailabilityType.UP;
 	};
+	var _exists = function() {
+		return find().size() == 1;
+	};
 	return {
 		getId : function() {return _id;},
 		toString : function() {return _res.toString();},
@@ -188,16 +221,94 @@ var Resource = function (param) {
 			common.trace("Resource("+_id+").getProxy()");
 			return ProxyFactory.getResource(_id);
 		},
-		remove : function() {},
+		/**
+		 * removes/deletes this resource from inventory. 
+		 * @returns true if resource no longer exists in inventory, false otherwise
+		 */
+		remove : function() {
+			common.trace("Resource("+_id+").remove()");
+			if (!_exists()) {
+				common.debug("Resource does not exists, nothing to remove");
+				return false;
+			}
+			try {
+				ResourceFactoryManager.deleteResource(_id);
+			} catch (exc) {
+				common.info("Cannot delete resource: "+exc);
+				return false;
+			}
+			return !common.waitFor(_exists);
+		},
+		/**
+		 * 
+		 * @param {Object} params
+		 * @returns array of child resources
+		 */
+		children : function(params) {
+			common.trace("Resource("+_id+").children("+common.objToString(params)+")");
+			params = params || {};
+			params.parentResourceId=_id;
+			return Inventory.find(params);
+		},
+		/**
+		 * 
+		 * @param {Object} params hashmap of filter params
+		 * @returns first matching child resource found
+		 */
+		child : function(params) {
+			common.trace("Resource("+_id+").child("+common.objToString(params)+")");
+			params = params || {};
+			params.parentResourceId=_id;
+			var children = Inventory.find(params);
+			if (children.length>0) {
+				return children[0];
+			}
+		},
+		createChild : function() {
+			
+		},
+		operations : function() {
+			
+		},
+		invokeOperation : function(name,params) {
+			common.trace("Resource("+_id+").invokeOperation(name="+name+",params={"+common.objToString(params)+"})");
+			var resOpShedule = OperationManager.scheduleResourceOperation(_id,name,0,0,0,0,null,null);
+			var opHistCriteria = new ResourceOperationHistoryCriteria();
+			opHistCriteria.addFilterJobId(resOpShedule.getJobId());
+			opHistCriteria.addFilterResourceIds(_id);
+			opHistCriteria.addSortStartTime(PageOrdering.DESC); // put most recent at top of results
+			opHistCriteria.setPaging(0, 1); // only return one result, in effect the latest
+			opHistCriteria.fetchResults(true);
+			var pred = function() {
+				var histories = OperationManager.findResourceOperationHistoriesByCriteria(opHistCriteria);
+				if (histories.size() > 0 && histories.get(0).getStatus() != OperationRequestStatus.INPROGRESS) {
+					return history.get(0);
+				}
+			}
+			var history = common.waitFor(pred);
+			return history;
+		},
+		/**
+		 * checks whether resource exists in inventory
+		 * @returns bool
+		 */
 		exists : function() {
 			common.trace("Resource("+_id+").exists()");
-			return find().size() == 1;
+			return _exists();
 		},
 		isAvailable : _isAvailable,
+		/**
+		 * wait's until resource becomes UP or timeout is reached
+		 * @returns true if became is available, false otherwise
+		 */
 		waitForAvailable : function() {
 			common.trace("Resource("+_id+").waitForAvailable()");
 			return common.waitFor(_isAvailable);
 		},
+		/**
+		 * unimports resource
+		 * @returns true if resource is dos no longer exist in inventory, false otherwise
+		 */
 		uninventory : function() {
 			common.trace("Resource("+_id+").uninventory()");
 			ResourceManager.uninventoryResources([_id]);
--- clitest/resources/js-files/commonmodule_test.js
+++ clitest/resources/js-files/commonmodule_test.js
@@ -1,19 +1,30 @@
 
 
 var verbose = 10;
+
+
 println(Inventory.discoveryQueue.list());
-println("disco queue listed");
+
 var newPlatforms = Inventory.discoveryQueue.listPlatforms();
 println(newPlatforms);
 if (newPlatforms.length>0) {
 	var platform = Inventory.discoveryQueue.importPlatform(newPlatforms[0].name);
+}
+var platforms = Inventory.platforms();
+
+if (platforms.length>0) {
+	platform = platforms[0];
 	println(platform);
+	platform.children();
 	println("Exists "+platform.exists());
 	println("Available "+platform.isAvailable());
 	platform.waitForAvailable();
 	println("Available "+platform.isAvailable());
 	println(Inventory.platforms());
+	println("Delete child "+platform.children()[0].remove());
+	platform.children({resourceTypeName:"RHQ Agent"});
 	platform.uninventory();
 	println(Inventory.platforms());
 	println("Exists "+ platform.exists());
 }
+	
