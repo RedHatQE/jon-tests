f94dd1bc60866df3b3b8013cd2ddd11a364195a2
Libor Zoubek <lzoubek@redhat.com>
2012-08-10 17:00:23 +0200

    UninventoryResource.js redone - now it depends on commonmodule.js and uninventories all platform children prior platforms itself

--- clitest/resources/js-files/UninventoryResources.js
+++ clitest/resources/js-files/UninventoryResources.js
@@ -1,29 +1,24 @@
 // With the help of this test case can uninventory all the resouce(s) from Server
 
 /**
- * @author jkandasa@redhat.com (Jeeva Kandasamy)
- * Apr 03, 2012
+ * @author lzoubek@redhat.com (Libor Zoubek)
+ * Aug 10, 2012
  */
 
 
-var criteria = new ResourceCriteria(); // Create new criteria
-criteria.addFilterResourceCategories(ResourceCategory.PLATFORM);
-var platformsDiscovered = ResourceManager.findResourcesByCriteria(criteria); // get All platforms
-println("Platforms(s)[#"+platformsDiscovered.size()+"]: "+platformsDiscovered);
+// this depends on commonmodule.js
 
-var i=0;
-var platformsArray = new Array();
-for(i=0;i<platformsDiscovered.size();i++){ 
-	platformsArray[i] = platformsDiscovered.get(i).getId();
-}
-println("Platforms about to Uninventory: "+platformsArray);
-ResourceManager.uninventoryResources(platformsArray);
 
-// Sleeping 10s. Just to be sure
+// first we uninventory each platform child one-by-one, then platforms itself
+Inventory.platforms().forEach(function (p) {
+	p.children().forEach(function(child) {
+		child.uninventory();
+	});
+	p.uninventory();
+});
+
 println("Sleeping 10sec to sync!!");
 sleep(1000*10);
-
  
-platformsDiscovered = ResourceManager.findResourcesByCriteria(criteria); // get All platforms
-assertEquals(platformsDiscovered.getTotalSize(), 0, "Number of Discovered platforms,");
+assertEquals(Inventory.platforms().length, 0, "Number of Discovered platforms,");
 println("All Platforms successfully removed from server");
