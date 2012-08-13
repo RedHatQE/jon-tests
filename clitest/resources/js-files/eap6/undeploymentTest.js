06b38dbab6f174312afdefa200d7f01a9c7db048
Libor Zoubek <lzoubek@redhat.com>
2012-08-13 13:10:41 +0200

    JON EAP6 CLI: let undeploymentTest take advantage of eap6/{standalone|domain}/server.js

--- clitest/resources/js-files/eap6/undeploymentTest.js
+++ clitest/resources/js-files/eap6/undeploymentTest.js
@@ -2,6 +2,7 @@
  * this script tests undeployment of WAR file on AS7
  * @author lzoubek@redhat.com (Libor Zoubek)
  * Jul 12, 2012
+ *  * requires commonmodulejs, eap6/{standalone|domain}/server.js
  */
 
 /**
@@ -19,23 +20,8 @@
 var platform = agent;
 var content = deployment;
 
-var eap;
-var eaps = Inventory.find({resourceTypeName:"JBossAS7 Standalone Server",parentResourceName:platform});
-if (eaps.length>0) {
-	// EAP found in inventory
-	eap = eaps[0];
-}
-else {
-	// we import all resources from discoqueue and wait 'till AS7 becomes available	
-	Inventory.discoveryQueue.importResources().forEach(function(r) {
-		if (r.getProxy().resourceType.name=="JBossAS7 Standalone Server") {
-			eap = r;
-		}
-	});
-}
-assertTrue(eap!=null,"EAP6 or AS7 standalone server is required for this test!");
-eap.waitForAvailable();
-assertTrue(eap.isAvailable(),"AS7 Standalone server was imported, and is available!");
+var eap = getEAP(platform);
+
 
 var name = content.replace(/.*\//,'');
 var deployed = eap.child({resourceTypeName:"Deployment",name:name});
