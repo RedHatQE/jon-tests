0762b1dae6d67200cb89472aa47b082d8ca495bf
Jeeva Kandasamy <jkandasa@redhat.com>
2012-10-04 10:45:17 +0530

    Fix - dashboardTest

--- sahi/src/com/redhat/qe/jon/sahi/tests/DashboardTest.java
+++ sahi/src/com/redhat/qe/jon/sahi/tests/DashboardTest.java
@@ -4,94 +4,67 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.redhat.qe.jon.sahi.base.SahiTestScript;
-import com.redhat.qe.jon.sahi.tasks.SahiTasks;
 
-public class DashboardTest  extends SahiTestScript  {
-	
-	
+public class DashboardTest  extends SahiTestScript  {	
+
 	@Test (groups="DashboardTest")
 	public void viewDashboard() {
-		
-		sahiTasks.clickDashboardTopLevelMenu(); 
-             		
-	}
-	
-	@Test (groups="DashboardTest")
-	public void editDashboard() {
-		
-		//sahiTasks.editDashboard();
-             		
-	}
-	
-	@Test (groups="DashboardTest")
-	public void newDashboard() {
-		sahiTasks.newDashboard(); 
-             		
-	}
-	
-	
-	@Test (groups="DashboardTest")
-	public void messagePortletExists() {
-		
-		Assert.assertTrue(sahiTasks.messagePortletExists()); 
-             		
-	}
-	
-	@Test (groups="DashboardTest")
-	public void inventorySummaryPortletExists() {
-		 
-		Assert.assertTrue(sahiTasks.inventorySummaryPortletExists()); 
-             		
+		sahiTasks.clickDashboardTopLevelMenu();             		
 	}
 
-	@Test (groups="DashboardTest")
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
+	public void editDashboard() {		
+		sahiTasks.editDashboard();             		
+	}
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
+	public void newDashboard() {
+		sahiTasks.newDashboard();              		
+	}	
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
+	public void messagePortletExists() {
+		Assert.assertTrue(sahiTasks.messagePortletExists());             		
+	}
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
+	public void inventorySummaryPortletExists() {		 
+		Assert.assertTrue(sahiTasks.inventorySummaryPortletExists());             		
+	}
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
 	public void mashupPortletExists() {
-		Assert.assertTrue(sahiTasks.mashupPortletExists()); 
-             		
+		Assert.assertTrue(sahiTasks.mashupPortletExists());              		
 	}
-	
-	@Test (groups="DashboardTest")
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
 	public void recentAlertsPortletExists() {
-		Assert.assertTrue(sahiTasks.recentAlertsPortletExists());  
-             		
-	}
-	
-	
-	@Test (groups="DashboardTest")
+		Assert.assertTrue(sahiTasks.recentAlertsPortletExists());             		
+	}	
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
 	public void alertedOrUnavailableResourcesPortlet() {
-		Assert.assertTrue(sahiTasks.alertedOrUnavailableResourcesPortletExists());  
-             		
+		Assert.assertTrue(sahiTasks.alertedOrUnavailableResourcesPortletExists());             		
 	}
-	
-	@Test (groups="DashboardTest")
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
 	public void recentOperationsPortlet() {
-		Assert.assertTrue(sahiTasks.recentOperationsPortletExists());   
-             		
-	}
-	
-	
-	@Test (groups="DashboardTest")
+		Assert.assertTrue(sahiTasks.recentOperationsPortletExists());             		
+	}	
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
 	public void portletRefresh() {   
-          sahiTasks.messagePortletRefresh();   		
-	}
-	
-	
-	@Test (groups="DashboardTest")
-	public void portletSettings() {   
-           //todo  		
-	}
-	
-	
-		
-	@Test (groups="DashboardTest")
+		sahiTasks.messagePortletRefresh();   		
+	}		
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
 	public void verifyInventorySummaryPortlet() {   
-           sahiTasks.verifyInventorySummaryPortlet();
+		sahiTasks.verifyInventorySummaryPortlet();
 	}
-	
-	@Test (groups="DashboardTest")
+
+	@Test (groups="DashboardTest", dependsOnMethods={"viewDashboard"})
 	public void verifyTabName() {   
-           sahiTasks.verifyDefaultTabName();
-	}
-	
-	
+		sahiTasks.verifyDefaultTabName();
+	}	
+
 }
