b70d58f48ce4d80ed466e319a2012b38570755ce
Armine Hovsepyan <ahovsepy@redhat.com>
2012-10-18 16:38:57 +0200

    1. verification of AS Server presence test is added (not included into testng.xml) 2. Operation definition Get and Post rest tests added (included in testng.xml)

--- rest-java/src/main/java/com/redhat/qe/jon/rest/tasks/RestClient.java
+++ rest-java/src/main/java/com/redhat/qe/jon/rest/tasks/RestClient.java
@@ -6,6 +6,8 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.ws.rs.core.MultivaluedMap;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -16,152 +18,231 @@ import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 /**
  * @author jkandasa (Jeeva Kandasamy)
  * @since Dec 20, 2011
  */
 
-
-
 public class RestClient extends TestScript {
-	private static Logger _logger = Logger.getLogger(RestClient.class.getName());
-	
+	private static Logger _logger = Logger
+			.getLogger(RestClient.class.getName());
+
 	protected static Client client = null;
 	protected static WebResource webResource = null;
-	protected static String RESPONSE_STATUS_CODE= "reposonse.status.code";
-	protected static String RESPONSE_STATUS_MESSAGE= "response.status.message";
-	protected static String RESPONSE_DATE= "reposnse.date";
-	protected static String RESPONSE_CONTENT_LENGTH= "reponse.content.length";
-	protected static String RESPONSE_EXPIRES= "response.expires";
-	protected static String RESPONSE_CONTENT_TYPE= "response.content.type";
-	protected static String RESPONSE_CONTENT= "response.content";
-	protected static String RESPONSE_OTHER= "response.others";
-	protected static String RESPONSE_X_POWERED_BY= "response.x.powered.by";
+	protected static String RESPONSE_STATUS_CODE = "reposonse.status.code";
+	protected static String RESPONSE_STATUS_MESSAGE = "response.status.message";
+	protected static String RESPONSE_DATE = "reposnse.date";
+	protected static String RESPONSE_CONTENT_LENGTH = "reponse.content.length";
+	protected static String RESPONSE_EXPIRES = "response.expires";
+	protected static String RESPONSE_CONTENT_TYPE = "response.content.type";
+	protected static String RESPONSE_CONTENT = "response.content";
+	protected static String RESPONSE_OTHER = "response.others";
+	protected static String RESPONSE_X_POWERED_BY = "response.x.powered.by";
 	protected static String RESPONSE_SERVER = "response.server";
-	
+
 	protected static String LINKS = "links";
 	protected static String RESOURCE_ID = "resourceId";
 	protected static String PARENT_ID = "parentId";
-	
-	//Status.json page keys
-	protected static String STATUS_VALUES 	= "values";
-	protected static String SERVER_VERSION 	= "SERVER_VERSION";
-	protected static String BUILD_NUMBER 	= "BuildNumber";
-	
-	
-	protected static String SERVER_URI ;
-	protected static String URI_PREFIX = "/rest/1"; 
-	
-	public enum URIs
-	{
-		STATUS("/status"),
-		GROUP("/group"),
-		ALERT("/alert"),
-		ALERT_DEFINATION("alert/definition"),
-		USER_FAVORITES_RESOURCE("user/favorites/resource"),
-		PLATFORMS("/resource/platforms"),
-		CHILDREN_RESOURCE("/resource/@@id@@/children");			
-		
+	protected static String ID = "id";
+	protected static String NAME = "name";
+
+	// Status.json page keys
+	protected static String STATUS_VALUES = "values";
+	protected static String SERVER_VERSION = "SERVER_VERSION";
+	protected static String BUILD_NUMBER = "BuildNumber";
+
+	protected static String SERVER_URI;
+	protected static String URI_PREFIX = "/rest/1";
+
+	public enum URIs {
+		STATUS("/status"), 
+		GROUP("/group"), 
+		ALERT("/alert"), 
+		ALERT_DEFINATION("alert/definition"), 
+		USER_FAVORITES_RESOURCE("user/favorites/resource"), 
+		PLATFORMS("/resource/platforms"), 
+		OPERATION_DEFINITION("/operation/definition/@@id@@"), 
+		OPERATION_DEFINITIONS("/operation/definitions"), 
+		CHILDREN_RESOURCE("/resource/@@id@@/children");
+
 		String uri;
-		URIs(String uri)
-		{
-			this.uri=uri;
+
+		URIs(String uri) {
+			this.uri = uri;
 		}
+
 		public String getUri() {
 			return uri;
 		}
+
 		public void setUri(String uri) {
 			this.uri = uri;
 		}
-		public String getFullUri() {
-			return (URI_PREFIX+uri);
-		}
-	}
-	
-	public WebResource getWebResource(String url, String userName, String passWord){
-		_logger.info("URI: "+url);
-		if(webResource == null){
-			client = Client.create();
-			client.addFilter(new HTTPBasicAuthFilter(userName, passWord)); 
-			webResource = client.resource(url);
-			return webResource;
-		}else{
-			client.addFilter(new HTTPBasicAuthFilter(userName, passWord)); 
-			webResource = client.resource(url);
-			return webResource;
-		}
-	}	
-	
-	public WebResource getWebResource(String url){
-		_logger.info("URI: "+url);
-		if(webResource == null){
-			client = Client.create();
-			webResource = client.resource(url);
-			return webResource;
-		}else{
-			webResource = client.resource(url);
-			return webResource;
-		}
-	}	
 
-	public HashMap<String, Object> getReponse(WebResource webResource, String url){
-		_logger.info("URI: "+url);
-        ClientResponse clientResponse = webResource.path(url).get(ClientResponse.class );
-        HashMap<String, Object> response = new HashMap<String, Object>();
-        response.put(RESPONSE_STATUS_CODE, clientResponse.getStatus());
-        response.put(RESPONSE_STATUS_MESSAGE, clientResponse.getClientResponseStatus());
-        response.put(RESPONSE_CONTENT_LENGTH, clientResponse.getHeaders().get("Content-Length"));
-        response.put(RESPONSE_CONTENT_TYPE, clientResponse.getHeaders().get("Content-Type"));
-        response.put(RESPONSE_DATE, clientResponse.getHeaders().get("Date"));
-        response.put(RESPONSE_EXPIRES, clientResponse.getHeaders().get("Expires"));
-        response.put(RESPONSE_SERVER, clientResponse.getHeaders().get("Server"));
-        response.put(RESPONSE_X_POWERED_BY, clientResponse.getHeaders().get("X-Powered-By"));
-        response.put(RESPONSE_CONTENT, clientResponse.getEntity(String.class));
-        _logger.fine("Response: "+response);
-        return response ;
+		public String getFullUri() {
+			return (URI_PREFIX + uri);
+		}
 	}
-	
-	public JSONObject getJSONObject(String content) throws ParseException{
-		JSONParser parser=new JSONParser();
-		return (JSONObject)parser.parse(content);
+
+	public WebResource getWebResource(String url, String userName,
+			String passWord) {
+		_logger.info("URI: " + url);
+		if (webResource == null) {
+			client = Client.create();
+			client.addFilter(new HTTPBasicAuthFilter(userName, passWord));
+			webResource = client.resource(url);
+			return webResource;
+		} else {
+			client.addFilter(new HTTPBasicAuthFilter(userName, passWord));
+			webResource = client.resource(url);
+			return webResource;
+		}
 	}
-	
-	public JSONArray getJSONArray(String content) throws ParseException{
-		JSONParser parser=new JSONParser();
-		return (JSONArray)parser.parse(content);
+
+	public WebResource getWebResource(String url) {
+		_logger.info("URI: " + url);
+		if (webResource == null) {
+			client = Client.create();
+			webResource = client.resource(url);
+			return webResource;
+		} else {
+			webResource = client.resource(url);
+			return webResource;
+		}
 	}
-	
+
+	public HashMap<String, Object> getReponse(WebResource webResource,
+			String url) {
+		_logger.info("URI: " + url);
+		ClientResponse clientResponse = webResource.path(url).get(
+				ClientResponse.class);
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		response.put(RESPONSE_STATUS_CODE, clientResponse.getStatus());
+		response.put(RESPONSE_STATUS_MESSAGE,
+				clientResponse.getClientResponseStatus());
+		response.put(RESPONSE_CONTENT_LENGTH,
+				clientResponse.getHeaders().get("Content-Length"));
+		response.put(RESPONSE_CONTENT_TYPE,
+				clientResponse.getHeaders().get("Content-Type"));
+		response.put(RESPONSE_DATE, clientResponse.getHeaders().get("Date"));
+		response.put(RESPONSE_EXPIRES,
+				clientResponse.getHeaders().get("Expires"));
+		response.put(RESPONSE_SERVER, clientResponse.getHeaders().get("Server"));
+		response.put(RESPONSE_X_POWERED_BY,
+				clientResponse.getHeaders().get("X-Powered-By"));
+		response.put(RESPONSE_CONTENT, clientResponse.getEntity(String.class));
+		_logger.fine("Response: " + response);
+		return response;
+	}
+
+	public HashMap<String, Object> getReponseWithParams(
+			WebResource webResource, String url, MultivaluedMapImpl queryParams) {
+		// MultivaluedMap queryParams = new MultivaluedMapImpl();
+		// queryParams.add("param1", "val1");
+		// queryParams.add("param2", "val2");
+		ClientResponse clientResponse = webResource.path(url)
+				.queryParams(queryParams).get(ClientResponse.class);
+		_logger.info("URI: " + url);
+		// ClientResponse clientResponse =
+		// webResource.path(url).get(ClientResponse.class );
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		response.put(RESPONSE_STATUS_CODE, clientResponse.getStatus());
+		response.put(RESPONSE_STATUS_MESSAGE,
+				clientResponse.getClientResponseStatus());
+		response.put(RESPONSE_CONTENT_LENGTH,
+				clientResponse.getHeaders().get("Content-Length"));
+		response.put(RESPONSE_CONTENT_TYPE,
+				clientResponse.getHeaders().get("Content-Type"));
+		response.put(RESPONSE_DATE, clientResponse.getHeaders().get("Date"));
+		response.put(RESPONSE_EXPIRES,
+				clientResponse.getHeaders().get("Expires"));
+		response.put(RESPONSE_SERVER, clientResponse.getHeaders().get("Server"));
+		response.put(RESPONSE_X_POWERED_BY,
+				clientResponse.getHeaders().get("X-Powered-By"));
+		response.put(RESPONSE_CONTENT, clientResponse.getEntity(String.class));
+		_logger.fine("Response: " + response);
+		return response;
+	}
+
+	public HashMap<String, Object> postResponse(WebResource webResource,
+			String uri, MultivaluedMap queryParam) {
+
+		ClientResponse clientResponse = webResource.path(uri).queryParams(queryParam).post(ClientResponse.class);
+
+		_logger.log(Level.INFO, webResource.getURI().getQuery());
+		// ClientResponse clientResponse =
+		// webResource.path(uri).post(ClientResponse.class, name);
+		HashMap<String, Object> response = new HashMap<String, Object>();
+		response.put(RESPONSE_STATUS_CODE, clientResponse.getStatus());
+		response.put(RESPONSE_STATUS_MESSAGE,
+				clientResponse.getClientResponseStatus());
+		response.put(RESPONSE_CONTENT_LENGTH,
+				clientResponse.getHeaders().get("Content-Length"));
+		response.put(RESPONSE_CONTENT_TYPE,
+				clientResponse.getHeaders().get("Content-Type"));
+		// response.put(RESPONSE_DATE, clientResponse.getHeaders().get("Date"));
+		// response.put(RESPONSE_EXPIRES,
+		// clientResponse.getHeaders().get("Expires"));
+		// response.put(RESPONSE_SERVER,
+		// clientResponse.getHeaders().get("Server"));
+		// response.put(RESPONSE_X_POWERED_BY,
+		// clientResponse.getHeaders().get("X-Powered-By"));
+		response.put(RESPONSE_CONTENT, clientResponse.getEntity(String.class));
+		_logger.fine("Response: " + response);
+		return response;
+
+	}
+
+	public JSONObject getJSONObject(String content) throws ParseException {
+		JSONParser parser = new JSONParser();
+		return (JSONObject) parser.parse(content);
+	}
+
+	public JSONArray getJSONArray(String content) throws ParseException {
+		JSONParser parser = new JSONParser();
+		return (JSONArray) parser.parse(content);
+	}
+
 	@SuppressWarnings("rawtypes")
-	public StringBuffer getKeyValue(JSONObject map, StringBuffer keyValue){
-		if(keyValue  == null){
-			keyValue = new StringBuffer(); 
+	public StringBuffer getKeyValue(JSONObject map, StringBuffer keyValue) {
+		if (keyValue == null) {
+			keyValue = new StringBuffer();
 		}
 		Set keys = map.keySet();
 		Iterator itkey = keys.iterator();
-	    while (itkey.hasNext()) {
-	    	String key = ""+itkey.next();
-	    	try{
-	    		if(((JSONArray)map.get(key)).size() > 1){
-		    		for(Object obj : (JSONArray)map.get(key)){
-		    			getKeyValue((JSONObject)obj, keyValue);
-		    		}
-		    	}else{
-		    		keyValue.append(key).append("=").append(map.get(key)).append("\n");	
-		    	}
-	    	}catch(Exception ex){
-	    		if(ex.getMessage() == null){
-	    			_logger.log(Level.WARNING, "Exception --> "+ex.getMessage(), ex);
-	    		}else if(ex.getMessage().contains("cannot be cast to org.json.simple.JSONArray")){
-	    			keyValue.append(key).append("=").append(map.get(key)).append("\n");
-	    		}else{
-	    			_logger.log(Level.SEVERE, "Exception --> "+ex.getMessage(), ex);
-	    		}
-	    	}	    	
-	    }
-	    return keyValue;
+		while (itkey.hasNext()) {
+			String key = "" + itkey.next();
+			try {
+				if (((JSONArray) map.get(key)).size() > 1) {
+					for (Object obj : (JSONArray) map.get(key)) {
+						getKeyValue((JSONObject) obj, keyValue);
+					}
+				} else {
+					keyValue.append(key).append("=").append(map.get(key))
+							.append("\n");
+				}
+			} catch (Exception ex) {
+				if (ex.getMessage() == null) {
+					_logger.log(Level.WARNING,
+							"Exception --> " + ex.getMessage(), ex);
+				} else if (ex.getMessage().contains(
+						"cannot be cast to org.json.simple.JSONArray")) {
+					keyValue.append(key).append("=").append(map.get(key))
+							.append("\n");
+				} else {
+					_logger.log(Level.SEVERE,
+							"Exception --> " + ex.getMessage(), ex);
+				}
+			}
+		}
+		return keyValue;
 	}
-	public void printKeyValue(JSONObject map){
-		_logger.info("JSON Map: \n------------------------------------\n"+getKeyValue(map, null)+"------------------------------------");
+
+	public void printKeyValue(JSONObject map) {
+		_logger.info("JSON Map: \n------------------------------------\n"
+				+ getKeyValue(map, null)
+				+ "------------------------------------");
 	}
 }
--- rest-java/src/main/java/com/redhat/qe/jon/rest/tests/RestTest.java
+++ rest-java/src/main/java/com/redhat/qe/jon/rest/tests/RestTest.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.ws.rs.core.MultivaluedMap;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
@@ -16,6 +18,7 @@ import org.testng.annotations.Test;
 
 import com.redhat.qe.Assert;
 import com.redhat.qe.jon.rest.tasks.RestClient;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 /**
  * @author jkandasa (Jeeva Kandasamy)
@@ -106,4 +109,97 @@ public class RestTest extends RestClient{
 		}
 	}
 
+
+	/**
+	 * recursively checks all resources of platform and prints our AS server Id
+	 * @param parentId
+	 * @throws ParseException
+	 */
+	
+	@Parameters({ "parent.id"})
+	@Test (groups="restClientJava")
+	public void verifyASServerPresence(@Optional String parentId) throws ParseException{
+		HashMap<String, Object> result;
+		String childResourceId = "";
+		if(parentId == null){
+			_logger.log(Level.INFO, "There is no paltform ID, taking it...");
+			result = this.getReponse(webResource, URIs.PLATFORMS.getUri()+".json");
+			Assert.assertEquals(result.get(RESPONSE_STATUS_CODE), 200, "Response Code: "+result.get(RESPONSE_STATUS_CODE)+", Response Message: "+result.get(RESPONSE_STATUS_MESSAGE));
+			JSONArray jsonArray = this.getJSONArray(""+result.get(RESPONSE_CONTENT));
+			_logger.fine("Number of Resource(s): "+jsonArray.size());
+			Assert.assertTrue(jsonArray.size()>0, "Number of platform(s) [>0] : "+jsonArray.size());
+			JSONObject jsonObject;
+			jsonObject = (JSONObject) jsonArray.get(0);
+			this.printKeyValue(jsonObject);
+			parentId = ""+jsonObject.get(RESOURCE_ID);
+		}
+
+		result =  this.getReponse(webResource, URIs.CHILDREN_RESOURCE.getUri().replace("@@id@@", parentId)+".json");
+		Assert.assertEquals(result.get(RESPONSE_STATUS_CODE), 200, "Response Code: "+result.get(RESPONSE_STATUS_CODE)+", Response Message: "+result.get(RESPONSE_STATUS_MESSAGE));
+		JSONArray jsonArray = this.getJSONArray(""+result.get(RESPONSE_CONTENT));
+		_logger.fine("Number of Children: "+jsonArray.size());
+		JSONObject jsonObject;
+		for(int i=0; i<jsonArray.size();i++){
+			jsonObject = (JSONObject) jsonArray.get(i);
+			this.printKeyValue(jsonObject);
+			Assert.assertEquals(parentId, ""+jsonObject.get(PARENT_ID), "Parent ID validation");
+			if(jsonObject.get("typeName").toString().contains("JBossAS Server")){
+				 childResourceId =  ""+jsonObject.get("resourceId");
+				_logger.log(Level.INFO, "AS Server resourceId is ..."+ childResourceId);	
+			}
+			
+		}
+		
+		}
+	
+	@Parameters({ "parent.id"})
+	@Test (groups="restClientJava")
+	public void checkPlatformOperations(@Optional String parentId) throws ParseException{
+		HashMap<String, Object> result;
+		String childResourceId = "";
+		if(parentId == null){
+			_logger.log(Level.INFO, "There is no paltform ID, taking it...");
+			result = this.getReponse(webResource, URIs.PLATFORMS.getUri()+".json");
+			Assert.assertEquals(result.get(RESPONSE_STATUS_CODE), 200, "Response Code: "+result.get(RESPONSE_STATUS_CODE)+", Response Message: "+result.get(RESPONSE_STATUS_MESSAGE));
+			JSONArray jsonArray = this.getJSONArray(""+result.get(RESPONSE_CONTENT));
+			_logger.fine("Number of Resource(s): "+jsonArray.size());
+			Assert.assertTrue(jsonArray.size()>0, "Number of platform(s) [>0] : "+jsonArray.size());
+			JSONObject jsonObject;
+			jsonObject = (JSONObject) jsonArray.get(0);
+			this.printKeyValue(jsonObject);
+			parentId = ""+jsonObject.get(RESOURCE_ID);
+		}
+
+		//call get operation for platform
+		_logger.log(Level.INFO,"REQUEST IS ---" + webResource + "  URI -- " + URIs.OPERATION_DEFINITION.getUri() );
+		MultivaluedMapImpl queryParams = new MultivaluedMapImpl();
+		queryParams.add("resourceId", parentId);
+		result = this.getReponseWithParams(webResource, URIs.OPERATION_DEFINITIONS.getUri(), queryParams);
+		_logger.log(Level.INFO,"RESULT IS ---" + result );
+		
+		//get first operation ID and Name for Platform
+		JSONArray jsonArray = this.getJSONArray(""+result.get(RESPONSE_CONTENT));
+		JSONObject jsonObject;
+		jsonObject = (JSONObject) jsonArray.get(0);
+		this.printKeyValue(jsonObject);
+		String  operationId = ""+jsonObject.get(ID);
+		_logger.log(Level.INFO,"operationId IS ---" + operationId );
+		
+		String  operationName = ""+jsonObject.get(NAME);
+		_logger.log(Level.INFO,"operationId IS ---" + operationName );
+		
+		//call operation/definition with post
+		MultivaluedMap queryParam = new MultivaluedMapImpl();
+		queryParam.add("resourceId", parentId);
+
+		result = this.postResponse(webResource, URIs.OPERATION_DEFINITION.getUri().replace("@@id@@", operationId), queryParam);
+		_logger.log(Level.INFO, "reponse code is  ---  "+result.get(RESPONSE_STATUS_CODE));
+		}
+	
+	@Parameters({ "parent.id"})
+	@Test (groups="restClientJava")
+	public void checkGroup(@Optional String parentId) throws ParseException{
+		//GroupRest gr = new GroupRest();
+		}
+	
 }
