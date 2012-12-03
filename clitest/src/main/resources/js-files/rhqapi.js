f76cb01569003532eb99283eee0f21c15c7ab86b
Libor Zoubek <lzoubek@redhat.com>
2012-12-03 14:55:28 +0100

    JON QE API: define Array.prototype.map if not supported in runtime

--- clitest/src/main/resources/js-files/rhqapi.js
+++ clitest/src/main/resources/js-files/rhqapi.js
@@ -21,6 +21,39 @@ var p = function(object) {
 	}
 };
 
+//Production steps of ECMA-262, Edition 5, 15.4.4.19
+//Reference: http://es5.github.com/#x15.4.4.19
+if (!Array.prototype.map) {
+	Array.prototype.map = function(callback, thisArg) {
+		var T, A, k;
+		if (this == null) {
+			throw new TypeError(" this is null or not defined");
+		}
+		var O = Object(this);
+		var len = O.length >>> 0;
+		if (typeof callback !== "function") {
+			throw new TypeError(callback + " is not a function");
+		}
+		if (thisArg) {
+			T = thisArg;
+		}
+		A = new Array(len);
+		k = 0;
+		while (k < len) {
+			var kValue, mappedValue;
+			if (k in O) {
+				kValue = O[k];
+				mappedValue = callback.call(T, kValue, k, O);
+				A[k] = mappedValue;
+			}
+			k++;
+		}
+		return A;
+	};
+}
+
+
+
 /**
  * @ignore this common module is instantiated by most of modules as private var
  */
