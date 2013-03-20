bd8ced92bda72cc9b81187713d9be6a1abfdc1f9
Libor Fuka <lfuka@dhcp-4-207.brq.redhat.com>
2013-03-20 15:43:46 +0100

    checkbox wait time 1s

--- sahi/src/main/java/com/redhat/qe/jon/sahi/base/inventory/Editor.java
+++ sahi/src/main/java/com/redhat/qe/jon/sahi/base/inventory/Editor.java
@@ -204,7 +204,7 @@ public class Editor {
      * @param check true to check, false to uncheck
      */
     public void checkBox(int index, boolean check) {
-        tasks.waitFor(Timing.WAIT_TIME);
+        tasks.waitFor(Timing.TIME_1S);
         String checkBox = null;
         if (check) {
             checkBox = "unchecked.png[" + index + "]";
