7f3aac348ae455639203a4bacf00b5232f440425
Filip Brychta <fbrychta@redhat.com>
2013-04-03 15:39:08 +0200

    CLI - increasing tolerance in cron scheduling test

--- clitest/src/main/resources/js-files/operations/groupOpScheduling.js
+++ clitest/src/main/resources/js-files/operations/groupOpScheduling.js
@@ -98,9 +98,9 @@ for(i in agents){
 	var firstOpCreatedTime = new Date(firstOpCreatedTimeMilis);
 	
 	assertTrue(firstOpCreatedTime > justBeforeScheduleTimeStamp && 
-			firstOpCreatedTimeMilis <justBeforeScheduleTimeStamp.getTime() + 12000, 
+			firstOpCreatedTimeMilis <justBeforeScheduleTimeStamp.getTime() + 14000, 
 			"First scheduled operation was created in incorrect timerange. Expected: "
-			+justBeforeScheduleTimeStamp +" - "+ new Date(justBeforeScheduleTimeStamp.getTime() + 12000)
+			+justBeforeScheduleTimeStamp +" - "+ new Date(justBeforeScheduleTimeStamp.getTime() + 14000)
 			+", actual time of scheduled operation: " + firstOpCreatedTime);
 	
 	// check that first operation was created according to cron expression (each 10 seconds) 
