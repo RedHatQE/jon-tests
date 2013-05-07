a5fc7fd3667c349f221c1f61404725051a35e2b5
Libor Zoubek <lzoubek@redhat.com>
2013-05-07 13:16:35 +0200

    rest-python: enable logging on console when running in interactive shell

--- rest-python/run.py
+++ rest-python/run.py
@@ -1,8 +1,7 @@
 def run_tests():
     from proboscis import TestProgram
     from tests import *
-
-                # Run Proboscis and exit.
+    # Run Proboscis and exit.
     TestProgram().run_and_exit()
 
 if __name__ == '__main__':
