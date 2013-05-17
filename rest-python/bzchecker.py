1a7ba89a80f8a48086a7bc63b0e9508cec9d210c
Libor Zoubek <lzoubek@redhat.com>
2013-05-17 17:19:54 +0200

    rest-python: improve BzChecker output

--- rest-python/bzchecker.py
+++ rest-python/bzchecker.py
@@ -3,7 +3,7 @@ import json
 import os
 from proboscis import SkipTest
 
-BUGZILLA='https://bugzilla.redhat.com/jsonrpc.cgi'
+BUGZILLA='https://bugzilla.redhat.com/'
 class blockedBy(object):
     def __init__(self,bz):
         self.bz = bz
@@ -12,10 +12,11 @@ class blockedBy(object):
         def wrap(*args):
             if os.getenv('BZCHECK'):
                 json_data = json.dumps([{'ids':self.bz}])
-                r = requests.get(BUGZILLA+'?method=Bug.get&params=%s' % json_data)
+                r = requests.get(BUGZILLA+'jsonrpc.cgi?method=Bug.get&params=%s' % json_data)
                 for bug in r.json()['result']['bugs']:
                     if bug['status'] in ['NEW','MODIFIED','ASSIGNED','ON_DEV']:
-                        raise SkipTest('BZ%s %s - %s' %(bug['id'],bug['status'],bug['summary']))
+                        bzLink = '%sshow_bug.cgi?id=%s' % (BUGZILLA,bug['id'])
+                        raise SkipTest('BZ [%s] %s (%s)' % (bug['status'],bug['summary'],bzLink))
             f(*args)
         wrap.func_name = f.func_name
         return wrap
