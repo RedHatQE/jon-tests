f73d1b4b9ef92a67346063c0f6816b7b1101654f
Libor Zoubek <lzoubek@redhat.com>
2013-05-24 16:16:19 +0200

    rest-python: fix bzChecker to handle case when sending request bugzilla fails

--- rest-python/bzchecker.py
+++ rest-python/bzchecker.py
@@ -2,6 +2,7 @@ import requests
 import json
 import os
 from proboscis import SkipTest
+import traceback
 
 BUGZILLA='https://bugzilla.redhat.com/'
 class blockedBy(object):
@@ -12,7 +13,13 @@ class blockedBy(object):
         def wrap(*args):
             if os.getenv('BZCHECK'):
                 json_data = json.dumps([{'ids':self.bz}])
-                r = requests.get(BUGZILLA+'jsonrpc.cgi?method=Bug.get&params=%s' % json_data)
+                try:
+                    r = requests.get(BUGZILLA+'jsonrpc.cgi?method=Bug.get&params=%s' % json_data)
+                except:
+                    print 'Could connect to %s not skipping test method' % BUGZILLA
+                    traceback.print_exc()
+                    f(*args)
+                    return
                 for bug in r.json()['result']['bugs']:
                     if bug['status'] in ['NEW','MODIFIED','ASSIGNED','ON_DEV']:
                         bzLink = '%sshow_bug.cgi?id=%s' % (BUGZILLA,bug['id'])
