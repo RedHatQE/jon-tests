77fb43621de10abfbce29476a81ab405fe689580
Libor Zoubek <lzoubek@redhat.com>
2013-06-05 12:47:58 +0200

    rest-python: linkchecker now also goes through json and xml

--- rest-python/tests/linkchecker.py
+++ rest-python/tests/linkchecker.py
@@ -21,20 +21,37 @@ class LinkCheckerTest(RHQRestTest):
     @test
     @blockedBy(['970592','970593'])
     def visit_all_html(self):
+        self.visited = {}
         with asserts.Check() as check:
-            r = self.visit_page(check,'','')
+            r = self.visit_page(check,'','',accepts='text/html',regex='<a href=\"([^\"]+)')
         self.log.info('DONE!! Visited %d pages' % len(self.visited))
 
-    def visit_page(self,check,referer,url,follow=True):
+    @test
+    def visit_all_json(self):
+        self.visited = {}
+        with asserts.Check() as check:
+            r = self.visit_page(check,'','',accepts='application/json',regex='href\"\:\"([^\"]+)')
+        self.log.info('DONE!! Visited %d pages' % len(self.visited))
+
+    @test
+    def visit_all_xml(self):
+        self.visited = {}
+        with asserts.Check() as check:
+            r = self.visit_page(check,'','',accepts='application/xml',regex='href=\"([^\"]+)')
+        self.log.info('DONE!! Visited %d pages' % len(self.visited))
+    
+    def visit_page(self,check,referer,url,follow=True,accepts='text/html',regex=''):
         self.visited[url] = True
-        r = self.get(url,accepts='text/html')
+        r = self.get(url,accepts=accepts)
         self.log.info('Visiting %s'%url)
         check.equal(r.status_code,200,'Server returned %d on GET %s, refering from %s' %(r.status_code,url,referer))
         if follow:
-            for m in re.finditer('<a href=\"([^\"]+)',r.text,re.DOTALL | re.IGNORECASE):
+            for m in re.finditer(regex,r.text,re.DOTALL | re.IGNORECASE):
                 href = m.group(1)
                 if href.find('/') == 0:
                     href = self.server_url.rstrip('/')+href
+                if href.find('http') < 0:
+                    href = self.url(href)
                 if href.find(self.server_url) >= 0 and not self.visited.has_key(href):
-                    self.visit_page(check,url,href)
+                    self.visit_page(check,url,href,accepts=accepts,regex=regex)
 
