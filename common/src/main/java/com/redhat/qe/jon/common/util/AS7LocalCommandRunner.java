af351a88db6fce0dc28e6e80949e85aea0c699c7
Radim Hatlapatka <rhatlapa@redhat.com>
2013-06-13 16:50:12 +0200

    Minor fix in command determining if AS7 is running or not on windows

--- common/src/main/java/com/redhat/qe/jon/common/util/AS7LocalCommandRunner.java
+++ common/src/main/java/com/redhat/qe/jon/common/util/AS7LocalCommandRunner.java
@@ -131,7 +131,7 @@ public class AS7LocalCommandRunner extends LocalCommandRunner implements IAS7Com
         if (platform.isWindows()) {
             String winFilter = "Commandline like '%" + asIdentifier + "%' AND Commandline like '%java%'";
             if (serverConfig != null) {
-                winFilter += "AND Commandline like '%" + serverConfig + "%'";
+                winFilter += " AND Commandline like '%" + serverConfig + "%'";
             }
             String pids = runAndWait("wmic process where \"" + winFilter + " AND name!='WMIC.exe'\" get Processid").getStdout();
             running = !(pids.trim().isEmpty());
