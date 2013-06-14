73570259a317955e671c8fd0ffb744c0bb8e9106
Filip Brychta <fbrychta@redhat.com>
2013-06-14 10:04:47 +0200

    CLI - updating blocking bz ids in StressCliTest

--- clitest/src/main/java/com/redhat/qe/jon/clitest/tests/stress/StressCliTest.java
+++ clitest/src/main/java/com/redhat/qe/jon/clitest/tests/stress/StressCliTest.java
@@ -9,7 +9,7 @@ import com.redhat.qe.jon.clitest.tests.CliTest;
 
 public class StressCliTest extends CliTest {
 
-	@Test(groups={"blockedByBug-829399"})
+	@Test(groups={"blockedByBug-887861"})
 	public void deployLargeWarTest() throws IOException, CliTasksException{
 		runJSfile("stress/test_LargeWarDeploy.js",
 				"args-style=named bundle=/tmp/rh_dep1.war",
@@ -19,7 +19,7 @@ public class StressCliTest extends CliTest {
 				"/tmp/byebye.war");
 	}
 	
-	@Test(groups={"blockedByBug-829399"})
+	@Test(groups={"blockedByBug-887861"})
 	public void deployLargeBundleTest() throws IOException, CliTasksException{
 		runJSfile("stress/test_LargeBundleDeploy.js",
 				"args-style=named bundle=/tmp/large_bundle.zip",
