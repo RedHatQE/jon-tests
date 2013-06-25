292e7f759cd1ff018c653e09061cd9a7e74571b8
Filip Brychta <fbrychta@redhat.com>
2013-06-25 11:26:40 +0200

    CLI - deleteDynaGroup test - searching only for groups with Dynagroup prefix

--- clitest/src/main/resources/js-files/groups/deleteDynaGroupDef.js
+++ clitest/src/main/resources/js-files/groups/deleteDynaGroupDef.js
@@ -20,5 +20,5 @@ removeAllDynaGroupDefs();
 var defs = dynaGroupDefs.findDynaGroupDefinitions();
 assertTrue(defs.length == 0,"Expected number of all dynaGroup definitions is: 0, but actual is: "+defs.length);
 // assert all groups are gone
-var allGroups = groups.find();
-assertTrue(allGroups.length == 0,"Expected number of all groups is: 0, but actual is: "+allGroups.length);
+var allGroups = groups.find({name:"DynaGroup"});
+assertTrue(allGroups.length == 0,"Expected number of all dyna groups is: 0, but actual is: "+allGroups.length);
