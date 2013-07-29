dcbe27505da04fbab9974c52f63abe961d5b97aa
Filip Brychta <fbrychta@redhat.com>
2013-07-29 15:59:15 +0200

    CLI - alert definition layer for rhqapi added #2

--- clitest/src/test/resources/js-files/alertDefinition/testStorageNodeAlertDefinition.js
+++ clitest/src/test/resources/js-files/alertDefinition/testStorageNodeAlertDefinition.js
@@ -2,15 +2,10 @@
 var resourceList = resources.find({pluginName:"RHQStorage", resourceTypeName:"VM Memory System"});
 assertTrue(resourceList.length > 0, "RHQStorage VM Memory System does not exist");
 
-//getting alert definition template of VM Memory Subsystem resource
-var alertList = alerts.findAlertDefinition({alertTemplateResourceTypeId:resourceList[0].getResourceTypeId()});
-var alert = alertList.get(0);
 
-//verifying alert definition name and condition
-assertTrue(alert.enabled, "Alert for VM Memory System is disabled.");
-assertTrue(alert.getName() == "StorageNodeHighHeapTemplate", "Alert name is not StorageNodeHighHeapTemplate.");
+var newAlertList = alertDefinitions.find({alertTemplateResourceTypeId:resourceList[0].getResourceTypeId()})
+//var newAlert = newAlertList.get(0);
+var newAlert = newAlertList[0];
 
-var conditionList = alert.getConditions().toArray();
-var condition = conditionList[0];
-assertTrue(condition.getComparator() == ">", "Alert comparator does not use >.");
-assertTrue(condition.getThreshold() == 0.75, "Alert threshold was not 75.0 %.");
+assertTrue(newAlert.conditions({})[0].comparator == ">", "Alert comparator does not use >.");
+assertTrue(newAlert.conditions({})[0].threshold == 0.75, "Alert threshold was not 75.0 %.");
