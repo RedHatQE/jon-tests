3b63ee7af6d421f7925d91037669193a10c69b76
Filip Brychta <fbrychta@redhat.com>
2013-10-29 16:23:17 +0100

    CLI - prioritize Eap5JonDocExampleCli tests

--- clitest/src/test/java/com/redhat/qe/jon/clitest/tests/docexample/Eap5JonDocExampleCliTest.java
+++ clitest/src/test/java/com/redhat/qe/jon/clitest/tests/docexample/Eap5JonDocExampleCliTest.java
@@ -7,12 +7,7 @@ import com.redhat.qe.jon.clitest.base.CliEngine;
 
 public class Eap5JonDocExampleCliTest extends CliEngine {
 
-	@Test
-	public void gettingDataForSingleAndMultipleResTest(){
-		createJSRunner("docExample/jon/short/gettingDataForSingleAndMultipleRes.js").
-			dependsOn("rhqapi.js").
-			run();
-	}
+	
 	
 	@Test
 	public void creatingContentBackedResTest(){
@@ -21,10 +16,24 @@ public class Eap5JonDocExampleCliTest extends CliEngine {
 			withResource("/deployments/MiscBeans-3.2.2.ear","deployment").
 			run();
 	}
+	@Test(dependsOnMethods = { "creatingContentBackedResTest" },
+			priority=1)
+	public void startingArrayTest(){
+		createJSRunner("docExample/jon/short/startingArray.js").
+			dependsOn("rhqapi.js").
+			run();
+	}
+	@Test(dependsOnMethods = { "creatingContentBackedResTest" },
+			priority=2)
+	public void gettingDataForSingleAndMultipleResTest(){
+		createJSRunner("docExample/jon/short/gettingDataForSingleAndMultipleRes.js").
+			dependsOn("rhqapi.js").
+			run();
+	}
 	
 	@Test(groups={"blockedByBug-851145"},
 			dependsOnMethods = { "creatingContentBackedResTest" },
-			priority=1)
+			priority=3)
 	public void updatingContentBackedResTest(){
 		createJSRunner("docExample/jon/short/updatingContentBackedRes.js").
 			dependsOn("rhqapi.js").
@@ -33,17 +42,12 @@ public class Eap5JonDocExampleCliTest extends CliEngine {
 	}
 	
 	@Test(dependsOnMethods = { "creatingContentBackedResTest" },
-			priority=2)
+			priority=4)
 	public void deletingContentBackedResTest(){
 		createJSRunner("docExample/jon/short/deletingContentBackedRes.js").
 			dependsOn("rhqapi.js").
 			run();
 	}
 	
-	@Test
-	public void startingArrayTest(){
-		createJSRunner("docExample/jon/short/startingArray.js").
-			dependsOn("rhqapi.js").
-			run();
-	}
+	
 }
