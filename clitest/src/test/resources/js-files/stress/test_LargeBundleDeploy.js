ee0cb9b30d3e3fe8e8da75a68759fdeb567b989f
Libor Zoubek <lzoubek@redhat.com>
2013-11-25 13:21:04 +0100

    clitest: add test for deploying large bundle when initially assigned to bundleGroup, also assert deployment result (so we can be more sure large deployment appeard on agent as well)

--- clitest/src/test/resources/js-files/stress/test_LargeBundleDeploy.js
+++ clitest/src/test/resources/js-files/stress/test_LargeBundleDeploy.js
@@ -5,6 +5,8 @@ var filePath = bundle;
 
 var eap = findStandaloneEAP6();
 
+var bundleGroup = toGroup == "true";
+
 var group="standalone-eap";
 
 println("Removing all existing bundles");
@@ -19,6 +21,15 @@ groups.find({name:group}).forEach(function(x) {
 var group = groups.create(group,[eap]);
 
 println("Creating bundle from dist-file");
-var b = bundles.createFromDistFile(filePath);
-var d = b.createDestination(group,null,null,"Deploy Directory");
-b.deploy(d);
+if (bundleGroup) {
+    bundleGroups.find().forEach(function(bg) {bg.remove();});
+    var bg = bundleGroups.create("group for very large bundle");
+    var b = bundles.create({dist:filePath,groups:[bg]});
+    var d = b.createDestination(group,null,null,"Deploy Directory");
+    b.deploy(d);
+}
+else {
+    var b = bundles.create({dist:filePath});
+    var d = b.createDestination(group,null,null,"Deploy Directory");
+    b.deploy(d);
+}
