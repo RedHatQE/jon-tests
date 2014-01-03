3cfbfaaa5197ec916ac5b9c6e563d947fb9cecbf
Filip Brychta <fbrychta@redhat.com>
2014-01-03 15:51:52 +0100

    CLI - UninventoryResources.js now reflects automatic re-inventory of RHQ Storage nodes

--- clitest/src/test/resources/js-files/inventory/UninventoryResources.js
+++ clitest/src/test/resources/js-files/inventory/UninventoryResources.js
@@ -1,4 +1,6 @@
-// With the help of this test case can uninventory all (except RHQ Storage Node)the resouce(s) from Server
+/** With the help of this test case can uninventory all the resouce(s) (except the RHQ Storage Nodes 
+ *  which are automatically re-inventoried) from Server
+ */
 
 /**
  * @author lzoubek@redhat.com (Libor Zoubek)
@@ -9,6 +11,8 @@ verbose = 3; // logging level
 
 // this depends on rhqapi.js
 
+// get all imported RHQ Storage Nodes
+var rhqStorageNodes = resources.find({resourceTypeName:"RHQ Storage Node"});
 
 // first we uninventory each platform child one-by-one, then platforms itself
 Inventory.platforms().forEach(function (p) {
@@ -18,9 +22,10 @@ Inventory.platforms().forEach(function (p) {
 	p.uninventory();
 });
 
-println("Sleeping 10sec to sync!!");
-sleep(1000*10);
+println("Sleeping 20sec to sync!!");
+sleep(1000*20);
  
-// 1 platform always stays in inventory because the resource RHQ Storage Node can't be uninventorized
-assertEquals(Inventory.platforms().length, 1, "Number of Discovered platforms,");
+// platforms which contain RHQ Storage Node always stays in inventory because the resource RHQ Storage Node is automatically re-inventoried
+assertEquals(Inventory.platforms().length, rhqStorageNodes.length, "Number of Discovered platforms should be " +rhqStorageNodes.length+
+        ", but actual number is " + Inventory.platforms().length);
 println("Platforms successfully removed from server");
