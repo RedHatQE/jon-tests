d181e1eb28a29f96a24ffb7a36823adbbec02186
Filip Brychta <fbrychta@redhat.com>
2014-04-07 17:19:38 +0200

    SAHI - added page object for Groups - draft

---
+++ sahi/src/main/java/com/redhat/qe/jon/sahi/base/inventory/groups/AllGroupsPage.java
@@ -0,0 +1,84 @@
+package com.redhat.qe.jon.sahi.base.inventory.groups;
+
+import java.util.logging.Logger;
+
+import net.sf.sahi.client.ElementStub;
+
+import com.redhat.qe.jon.sahi.base.editor.Editor;
+import com.redhat.qe.jon.sahi.tasks.SahiTasks;
+import com.redhat.qe.jon.sahi.tasks.Timing;
+
+public class AllGroupsPage {
+    private static Logger log = Logger.getLogger(AllGroupsPage.class.getName());
+    protected SahiTasks tasks = null;
+    protected Editor editor = null;
+    
+    public AllGroupsPage(SahiTasks tasks) {
+        this.tasks = tasks;
+        this.editor = new Editor(tasks);
+    }
+    
+    /**
+     * Navigates to the page
+     */
+    public AllGroupsPage navigate(){
+        String serverBaseUrl = tasks.getNavigator().getServerBaseUrl();
+        String url = serverBaseUrl+"/#Inventory/Groups/AllGroups";
+        log.fine("Navigating to ["+url+"]");
+        tasks.navigateTo(url,false);
+        tasks.waitForElementVisible(tasks, tasks.cell("New"), "New button", Timing.WAIT_TIME);
+        
+        return this;
+    }
+    
+    public AllGroupsPage createNewGroup(Group newGroup){
+        tasks.cell("New").click();
+        tasks.waitForElementVisible(tasks, tasks.cell("Create Group"), "Create Group label", Timing.WAIT_TIME);
+        
+        if(newGroup.getName() != null){
+            log.info("Creating a group named " + newGroup.getName());
+            tasks.textbox("name").setValue(newGroup.getName());
+        }
+        if(newGroup.getDescription() != null){
+            tasks.textarea("description").setValue(newGroup.getDescription());
+        }
+
+        if(newGroup.isRecursive()){
+            editor.checkBox(0, true);
+        }
+        tasks.cell("Next").click();
+        tasks.waitForElementVisible(tasks, tasks.cell("Available resource"), "Available resource label", Timing.WAIT_TIME);
+        
+        // select resources which will belong to this group
+        for (String resName : newGroup.getResourceNames()){
+            ElementStub elem =tasks.textbox("/textItem.*/").near(tasks.cell("Search :[1]"));
+            elem.setValue(resName);
+            // click there to invoke filtering
+            elem.click();
+            
+            tasks.waitFor(Timing.WAIT_TIME);
+            tasks.div(resName).click();
+            tasks.image("right.png").click();
+        }
+        
+        tasks.cell("Finish").click();
+        tasks.waitForElementVisible(tasks, tasks.cell("You have created a new resource group with name ["+newGroup.getName()+"]."), 
+                "Available resource label", Timing.WAIT_TIME);
+        // TODO check errors
+        
+        return this;
+    }
+    
+    public AllGroupsPage deleteGroup(String groupName){
+        if(tasks.div(groupName).isVisible()){
+            log.info("Removing a group named " + groupName);
+            tasks.div(groupName).click();
+            tasks.cell("Delete").click();
+            tasks.cell("Yes").click();
+            tasks.waitForElementVisible(tasks, tasks.cell("You have successfully deleted the selected resource groups"),
+                    "User was deleted message", Timing.WAIT_TIME);
+            // TODO check errors
+        }
+        return this;
+    }
+}
---
+++ sahi/src/main/java/com/redhat/qe/jon/sahi/base/inventory/groups/Group.java
@@ -0,0 +1,39 @@
+package com.redhat.qe.jon.sahi.base.inventory.groups;
+
+import java.util.ArrayList;
+
+public class Group {
+    private String name = null;
+    private String description = null;
+    private boolean isRecursive = false;
+    private ArrayList<String> resourceNames = new ArrayList<String>();
+    
+    public  Group(String name){
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public boolean isRecursive() {
+        return isRecursive;
+    }
+    public void setRecursive(boolean isRecursive) {
+        this.isRecursive = isRecursive;
+    }
+    public ArrayList<String> getResourceNames() {
+        return resourceNames;
+    }
+    public void setResourceNames(ArrayList<String> resourceNames) {
+        this.resourceNames = resourceNames;
+    }
+
+}
