211520d3ddb659e5b5d24dc3fc3b275623199186
Jeeva Kandasamy <jkandasa@redhat.com>
2014-04-21 18:30:53 +0530

    SAHI - 'postgres' resource name changed as 'Postgres' in recent builds

--- sahi/src/main/java/com/redhat/qe/jon/sahi/base/postgresplugin/PostgresPluginBase.java
+++ sahi/src/main/java/com/redhat/qe/jon/sahi/base/postgresplugin/PostgresPluginBase.java
@@ -22,7 +22,7 @@ abstract class PostgresPluginBase {
 	}
 	
 	public void navigateToPostgresBase() {
-	    Resource agent = new Resource(tasks,System.getProperty("jon.agent.host"),"postgres");
+	    Resource agent = new Resource(tasks,System.getProperty("jon.agent.name"),"Postgres");
 	    agent.navigate();
 	    tasks.waitForElementVisible(tasks, tasks.cell("/Databases/"), "Databases Visible",Timing.TIME_5S);
 	}
