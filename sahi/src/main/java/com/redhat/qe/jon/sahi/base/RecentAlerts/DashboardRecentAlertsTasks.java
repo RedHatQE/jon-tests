597a5c424254704eb9f1d27031656d8ac27b5eb0
Matt Mahoney <mmahoney@redhat.com>
2014-04-25 15:17:02 -0400

    main/java/com/redhat/qe/jon/sahi/base/RecentAlerts/DashboardRecentAlertsTasks.java

---
+++ sahi/src/main/java/com/redhat/qe/jon/sahi/base/RecentAlerts/DashboardRecentAlertsTasks.java
@@ -0,0 +1,39 @@
+package com.redhat.qe.jon.sahi.base.RecentAlerts;
+
+import org.testng.Assert;
+import com.redhat.qe.jon.sahi.tasks.SahiTasks;
+import com.redhat.qe.jon.sahi.tasks.Timing;
+
+/**
+ * @author mmahoney
+ */
+
+public class DashboardRecentAlertsTasks extends RecentAlertsBase {
+	
+	public DashboardRecentAlertsTasks(SahiTasks sahiTasks) {
+		super(sahiTasks);
+		tasks = sahiTasks;
+	}
+	
+	public void navigateToDashboardDefaultView() {
+		tasks.waitForElementVisible(tasks, tasks.link("Dashboard"), "Dashboard.", Timing.TIME_5S);
+		tasks.link("Dashboard").click();
+		tasks.waitForElementVisible(tasks, tasks.cell("Default"), "Default", Timing.TIME_10S);
+		tasks.cell("Default").click();
+	}
+	
+	public boolean addRecentAlertsPortlet() {
+		
+		// Add the Recent Alerts Portlet if it does not already exist
+		if (!tasks.cell("Recent Alerts").exists()) {
+				
+			this.putIntoEditMode("New Dashboard");
+			tasks.waitForElementVisible(tasks, tasks.cell("Add Portlet"), "Add Portlet.", Timing.TIME_5S);
+
+			selectMenu("Add Portlet", "Recent Alerts");
+			Assert.assertTrue(tasks.waitForElementVisible(tasks, tasks.cell("Recent Alerts"), "Recent Alerts.", Timing.TIME_10S));
+		}
+		
+		return true;
+	}
+}
