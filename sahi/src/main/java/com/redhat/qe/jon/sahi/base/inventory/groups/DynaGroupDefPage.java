e77a7f6c51f16c6201047940cf4e2e964a0b63be
Filip Brychta <fbrychta@redhat.com>
2014-05-19 12:35:14 +0200

    SAHI - added PredefinedDynagroupDefTest

---
+++ sahi/src/main/java/com/redhat/qe/jon/sahi/base/inventory/groups/DynaGroupDefPage.java
@@ -0,0 +1,111 @@
+package com.redhat.qe.jon.sahi.base.inventory.groups;
+
+import java.util.logging.Logger;
+
+import com.redhat.qe.jon.sahi.base.editor.Editor;
+import com.redhat.qe.jon.sahi.tasks.SahiTasks;
+import com.redhat.qe.jon.sahi.tasks.Timing;
+
+/**
+ * This class represents /#Inventory/Groups/DynagroupDefinitions page.
+ * @author fbrychta
+ *
+ */
+public class DynaGroupDefPage {
+    private static Logger log = Logger.getLogger(DynaGroupDefPage.class.getName());
+    protected SahiTasks tasks = null;
+    protected Editor editor = null;
+    public static final String COMBOBOX_SELECTOR = "/selectItemText.*/";
+
+    public DynaGroupDefPage(SahiTasks tasks) {
+        this.tasks = tasks;
+        this.editor = new Editor(tasks);
+    }
+
+    /**
+     * Navigates to the page
+     * @return this
+     */
+    public DynaGroupDefPage navigate(){
+        String serverBaseUrl = tasks.getNavigator().getServerBaseUrl();
+        String url = serverBaseUrl+"/#Inventory/Groups/DynagroupDefinitions";
+        log.fine("Navigating to ["+url+"]");
+        tasks.navigateTo(url,false);
+        tasks.waitForElementVisible(tasks, tasks.cell("New"), "New button", Timing.WAIT_TIME);
+
+        return this;
+    }
+
+    /**
+     * Creates a new dynagroup definition with given parameters.
+     * @return true if given definition was successfully created, false otherwise
+     */
+    public boolean createNew(DynagroupDef def){
+        tasks.click(tasks.cell("New"));
+        tasks.waitForElementVisible(tasks, tasks.cell("Save"), "Save button", Timing.WAIT_TIME);
+        if(def.getProvidedExprName() != null){
+            tasks.selectComboBoxByNearCellOptionByDiv(tasks, COMBOBOX_SELECTOR, "Provided Expression :", def.getProvidedExprName());
+        }
+        if(def.getName() != null){
+            editor.setText("name", def.getName());
+        }
+        if(def.getDescription() != null){
+            editor.setTextInTextArea("description", def.getDescription());
+        }
+        if(def.getExpression() != null){
+            // TODO: \n is not working
+            editor.setTextInTextArea("expression", def.getExpression());
+        }
+        boolean isRecursive = !tasks.div("Recursive").containsHTML("unchecked");
+        if(def.isRecursive() != isRecursive){
+            tasks.div("Recursive").click();
+        }
+        if(def.getRecalcInt() >=0){
+            editor.setText("recalculationInterval", Integer.toString(def.getRecalcInt()));
+        }
+
+        log.fine("Saving a definition.");
+        tasks.waitForElementVisible(tasks, tasks.cell("Save"), "Save button", Timing.WAIT_TIME);
+        tasks.clickOnFirstVisibleElement(tasks.cell("Save"));
+        return tasks.waitForElementVisible(tasks, tasks.cell("/You have successfully saved the group definition named*/"), 
+                "Successful message",Timing.WAIT_TIME);
+    }
+    /**
+     * Tries to get parameters of given dynagroup definition.
+     * 
+     * @throws RuntimeException when a definition with given name is not found on the page
+     * @return DynagroupDef parsed definition with given name
+     */
+    public DynagroupDef getDefinition(String name){
+        if(tasks.link(name).isVisible()){
+            tasks.link(name).click();
+            tasks.waitForElementVisible(tasks, tasks.cell("Save"), "Save button", Timing.WAIT_TIME);
+            return new DynagroupDef(editor.getText("name"),
+                    editor.getTextInTextArea("description"),
+                    "",
+                    editor.getTextInTextArea("expression"),
+                    !tasks.div("Recursive").containsHTML("unchecked"),
+                    Integer.parseInt(editor.getText("recalculationInterval")));
+        }else{
+            throw new RuntimeException("Given dynagroup definition ["+name+"] was not found!!");
+        }
+    }
+    /**
+     * Deletes a dynagroup definition with given name.
+     * 
+     * @return true if the definition was successfully deleted, false when the 
+     * definition was not found or deletion failed
+     */
+    public boolean deleteDefinition(String name){
+        log.fine("Trying to delete a definition with name " + name);
+        if(tasks.div(name).isVisible()){
+            tasks.div(name).click();
+            tasks.click(tasks.cell("Delete"));
+            tasks.click(tasks.cell("Yes"));
+            return tasks.waitForElementVisible(tasks, tasks.cell("/You have successfully deleted [1] group definitions*/"), 
+                    "Successful message",Timing.WAIT_TIME);
+        }
+        return false;
+    }
+
+}
---
+++ sahi/src/test/java/com/redhat/qe/jon/sahi/tests/groups/PredefinedDynagroupDefTest.java
@@ -0,0 +1,173 @@
+package com.redhat.qe.jon.sahi.tests.groups;
+
+import java.util.logging.Logger;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.redhat.qe.jon.sahi.base.OnAgentSahiTestScript;
+import com.redhat.qe.jon.sahi.base.inventory.Resource;
+import com.redhat.qe.jon.sahi.base.inventory.groups.DynaGroupDefPage;
+import com.redhat.qe.jon.sahi.base.inventory.groups.DynagroupDef;
+
+/**
+ * This test creates all available canned expressions and checks if they were
+ * successfully created with correct parameters.
+ * @author fbrychta
+ *
+ */
+public class PredefinedDynagroupDefTest extends OnAgentSahiTestScript {
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+
+    @Test(dataProvider="cannedExprParameters")
+    public void useCannedExpressionsTest(String providedExprName,DynagroupDef expectedResult){
+        DynaGroupDefPage dynagroupDefPage = new DynaGroupDefPage(sahiTasks);
+        dynagroupDefPage.navigate();
+
+        // change a name to avoid conflicts with predefined dynagroups
+        String changedName = expectedResult.getName() +" - user";
+        dynagroupDefPage.deleteDefinition(changedName);
+        DynagroupDef testDef = new DynagroupDef();
+        testDef.setProvidedExprName(providedExprName);
+        testDef.setName(changedName);
+        Assert.assertTrue(dynagroupDefPage.createNew(testDef),"Definition named "+changedName+", was successfully created");
+
+        // try to find created definition and check it's parameters
+        dynagroupDefPage.navigate();
+        DynagroupDef parsedDef = dynagroupDefPage.getDefinition(changedName);
+        expectedResult.setName(changedName);
+        assertDefinitions(parsedDef,expectedResult);
+    }
+    @DataProvider
+    public Object[][] cannedExprParameters(){
+        return new Object[][] {
+                {"JBossAS - All hosting any version of "+'"'+"my"+'"'+" app",
+                    new DynagroupDef("All hosting any version of \"my\" app",
+                            "All hosting any version of \"my\" app",
+                            "",
+                            "resource.type.plugin = JBossAS\n"
+                            + "resource.type.name = JBossAS Server\n"
+                            + "resource.child.name.contains = my\n",
+                            false,
+                            10)},
+                {"JBossAS - All non-secured",
+                    new DynagroupDef("All non-secured",
+                            "All non-secured JBossAS4 servers",
+                            "",
+                            "empty resource.pluginConfiguration[principal]\n"
+                            + "resource.type.plugin = JBossAS\n"
+                            + "resource.type.name = JBossAS Server\n",
+                            false,
+                            10)},
+                {"JBossAS - Clustered EARs",
+                    new DynagroupDef("Clustered EARs",
+                            "Clustered EARs on JBoss AS4",
+                            "",
+                            "groupby resource.parent.trait[partitionName]\n"
+                            + "groupby resource.name\nresource.type.plugin = JBossAS\n"
+                            + "resource.type.name = Enterprise Application (EAR)\n",
+                            false,
+                            10)},
+                {"JBossAS - Clusters",
+                    new DynagroupDef("Clusters",
+                            "JBoss AS4 clusters",
+                            "",
+                            "groupby resource.trait[partitionName]\n"
+                            + "resource.type.plugin = JBossAS\n"
+                            + "resource.type.name = JBossAS Server\n",
+                            false,
+                            10)},
+                {"JBossAS - Unique versions",
+                    new DynagroupDef("Unique versions",
+                            "Unique versions of JBoss AS4",
+                            "",
+                            "groupby resource.trait[jboss.system:type=Server:VersionName]\n"
+                            + "resource.type.plugin = JBossAS\n"
+                            + "resource.type.name = JBossAS Server\n",
+                            false,
+                            10)},
+                {"JBossAS5 - Clusters",
+                    new DynagroupDef("Clusters",
+                            "JBoss AS5/6 clusters",
+                            "",
+                            "groupby resource.trait[MCBean|ServerConfig|*|partitionName]\n"
+                            + "resource.type.plugin = JBossAS5\n"
+                            + "resource.type.name = JBossAS Server\n",
+                            false,
+                            10)},
+                {"JBossAS7 - Enabled deployments in server-group",
+                    new DynagroupDef("Enabled deployments in server-group",
+                            "JBoss AS7 Deployments in server-groups",
+                            "",
+                            "groupby resource.parent.name\n"
+                            + "resource.type.plugin = JBossAS7\n"
+                            + "resource.type.name = Deployment\n"
+                            + "resource.parent.type.name.contains = ServerGroup\n"
+                            + "resource.resourceConfiguration[enabled] = true\n",
+                            false,
+                            10)},
+                {"JBossAS7 - Managed Servers in domain",
+                    new DynagroupDef("Managed Servers in domain",
+                            "JBoss AS7 Managed servers in server in domains",
+                            "",
+                            "groupby resource.resourceConfiguration[hostname]\n"
+                            + "resource.type.plugin = JBossAS7\n"
+                            + "resource.type.name = Managed Server\n",
+                            false,
+                            10)},
+                {"JBossAS7 - Managed Servers in server-group",
+                    new DynagroupDef("Managed Servers in server-group",
+                            "JBoss AS7 Managed servers in server in server-groups",
+                            "",
+                            "groupby resource.resourceConfiguration[group]\n"
+                            + "resource.type.plugin = JBossAS7\n"
+                            + "resource.type.name = Managed Server\n",
+                            false,
+                            10)},
+                {"Platforms - All resources currently down",
+                    new DynagroupDef("All resources currently down",
+                            "Group of all resources currently down",
+                            "",
+                            "resource.availability = DOWN\n",
+                            false,
+                            10)},
+                {"Platforms - Groups by platform",
+                    new DynagroupDef("Groups by platform",
+                            "Generates groups by platform",
+                            "",
+                            "resource.type.category = PLATFORM\n"
+                            + "groupby resource.type.name\n",
+                            false,
+                            10)},
+                {"Platforms - Unique resource types in inventory",
+                    new DynagroupDef("Unique resource types in inventory",
+                            "Unique resource types in inventory",
+                            "",
+                            "groupby resource.type.plugin\ngroupby resource.type.name\n",
+                            false,
+                            10)},
+                {"RHQAgent - All RHQ Agent resources in inventory",
+                    new DynagroupDef("All RHQ Agent resources in inventory",
+                            "Maintains a group of all RHQ agents in inventory",
+                            "",
+                            "resource.type.plugin = RHQAgent\n"
+                            + "resource.type.name = RHQ Agent\n",
+                            false,
+                            10)}
+        };
+    }
+    private void assertDefinitions(DynagroupDef actual, DynagroupDef expected){
+        log.fine("Checking dynagroup definition named: " + expected.getName());
+        Assert.assertTrue(actual.getName().equals(expected.getName()),
+                "Expected name is: "+expected.getName()+", but actual is: " +actual.getName());
+        Assert.assertTrue(actual.getDescription().equals(expected.getDescription()),
+                "Expected description is: "+expected.getDescription()+", but actual is: " +actual.getDescription());
+        Assert.assertTrue(actual.getExpression().equals(expected.getExpression()),
+                "Expected expression is: "+expected.getExpression()+", but actual is: " +actual.getExpression());
+        Assert.assertTrue(actual.isRecursive() == expected.isRecursive(),
+                "Expected recursivnes is: "+expected.isRecursive()+", but actual is: " +actual.isRecursive());
+        Assert.assertTrue(actual.getRecalcInt() == expected.getRecalcInt(),
+                "Expected recalc interval is: "+expected.getRecalcInt()+", but actual is: " +actual.getRecalcInt());
+    }
+}
