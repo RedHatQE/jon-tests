134da99d580262e4c3162b7b49205441809b4cfc
Filip Brychta <fbrychta@redhat.com>
2014-05-23 14:06:11 +0200

    CLI - removed eap6 related tests from dynagroupsExist test Eap6 tests were moved to jon-eap6 repo

--- clitest/src/test/resources/js-files/groups/predefinedDynaGroups/dynagroupsExist.js
+++ clitest/src/test/resources/js-files/groups/predefinedDynaGroups/dynagroupsExist.js
@@ -16,40 +16,27 @@ var common = new _common();
 var allLinuxPlat = resources.find({resourceTypeName:"Linux"});
 var allWinPlat = resources.find({resourceTypeName:"Windows"});
 var allResDown = resources.find({availability:"DOWN"});
-var allManagedServers = resources.find({pluginName:"JBossAS7",resourceTypeName:"Managed Server"});
 var allAgents = resources.find({pluginName:"RHQAgent",name:"RHQ Agent"});
 
 
 // arrays with input parameters
 var defNames = ["Groups by platform",
                 "All resources currently down",
-                "Managed Servers in domain",
-                "Managed Servers in server-group",
                 "All RHQ Agent resources in inventory"];
 var defDescriptions = ["Generates groups by platform",
                        "Group of all resources currently down",
-                       "JBoss AS7 Managed servers in server in domains",
-                       "JBoss AS7 Managed servers in server in server-groups",
                        "Maintains a group of all RHQ agents in inventory"];
 var expressions = ["resource.type.category = PLATFORM\n"+
                    "groupby resource.type.name",
 
                    "resource.availability = DOWN",
 
-                   "groupby resource.resourceConfiguration[hostname]\n"+
-                   "resource.type.plugin = JBossAS7\n"+
-                   "resource.type.name = Managed Server",
-
-                   "groupby resource.resourceConfiguration[group]\n"+
-                   "resource.type.plugin = JBossAS7\n"+
-                   "resource.type.name = Managed Server",
-                   
                    "resource.type.plugin = RHQAgent\n" +
                    "resource.type.name = RHQ Agent"
                    ];
-var isRecursive = [false,false,false,false,false];
+var isRecursive = [false,false,false];
 var tenMinutes = 1000 * 60 * 10;
-var recalInterval = [tenMinutes, tenMinutes, tenMinutes, tenMinutes, tenMinutes];
+var recalInterval = [tenMinutes, tenMinutes, tenMinutes];
 
 // get expected number of groups which will be managed by created dynaGroup definition
 var numberOfPlatformTypes = 0;
@@ -60,27 +47,7 @@ if(allWinPlat.length > 0){
     numberOfPlatformTypes++;
 }
 
-var numberOfManagedServersWithDifHostname = 0;
-var numberOfManagedServersWithDifGroup = 0;
-// TODO, cp this to EAP6 suite and fix Managed Servers in domain
-var foundHostnames = [];
-var foundGroups = [];
-for(var x in allManagedServers){
-    var conf = allManagedServers[x].getConfiguration();
-    var hostname = conf["hostname"];
-    common.trace("Host name: " + hostname);
-    var group = conf["group"];
-    common.trace("Group name: " + group);
-    if(foundHostnames.indexOf(hostname) == -1){
-        foundHostnames.push(hostname);
-        numberOfManagedServersWithDifHostname++;
-    }
-    if(foundGroups.indexOf(group) == -1){
-        foundGroups.push(group);
-        numberOfManagedServersWithDifGroup++;
-    }
-}
-var expectedNumberOfManagedGroups = [numberOfPlatformTypes, 1,numberOfManagedServersWithDifHostname,numberOfManagedServersWithDifGroup,1];
+var expectedNumberOfManagedGroups = [numberOfPlatformTypes, 1,1];
 
 
 // assert values
@@ -93,4 +60,4 @@ for (i in defNames) {
 // check if number of resources in created managed groups is correct
 checkNumberOfResourcesInGroup(getManagedGroup(defNames[0]), allLinuxPlat.length);
 checkNumberOfResourcesInGroup(getManagedGroup(defNames[1]), allResDown.length);
-checkNumberOfResourcesInGroup(getManagedGroup(defNames[4]), allAgents.length);
+checkNumberOfResourcesInGroup(getManagedGroup(defNames[2]), allAgents.length);
