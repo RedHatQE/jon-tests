5de909d811a88bdfa26097914d087da76fef08d0
Filip Brychta <fbrychta@redhat.com>
2014-08-04 11:53:18 +0200

    CLI - importPortServices is skipped for JON (not supported)

--- clitest/src/test/java/com/redhat/qe/jon/clitest/tests/importPortServices/ImportPortServicesTest.java
+++ clitest/src/test/java/com/redhat/qe/jon/clitest/tests/importPortServices/ImportPortServicesTest.java
@@ -1,5 +1,6 @@
 package com.redhat.qe.jon.clitest.tests.importPortServices;
 
+import org.testng.SkipException;
 import org.testng.annotations.Test;
 
 import com.redhat.qe.jon.clitest.base.CliEngine;
@@ -8,6 +9,9 @@ public class ImportPortServicesTest extends CliEngine {
 
 	@Test
 	public void importPortServices() {
+	    if(System.getProperty("rhq.build.version", "").contains("JON")){
+            throw new SkipException("rhq-netservices-plugin is not supported in JON");
+        }
 		createJSRunner("importPortServices/importPortServices.js").addDepends("rhqapi.js").run();
 	}
 }
